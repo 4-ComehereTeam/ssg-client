c47cce65d6df379d3180408090d61798544ddae8
ddolboghi <rhdfyd128@gmail.com>
2024-03-15 05:26:04 +0900

    feat: 로그인, 아이디/비밀번호 찾기, 본인 인증 페이지 생성

---
+++ src/app/member/certification/page.tsx
@@ -0,0 +1,7 @@
+import AgreeForm from "@/components/form/AgreeForm"
+
+function Page() {
+  return <AgreeForm />
+}
+
+export default Page
---
+++ src/app/member/find-id-pw/page.tsx
@@ -0,0 +1,8 @@
+import CertificationMethods from "@/components/ui/CertificationMethods"
+import React from "react"
+
+function Page() {
+  return <CertificationMethods />
+}
+
+export default Page
