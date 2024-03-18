6cd8ed86a3e33821c646ea0956f9fff296ba2876
sw-devr <sw4689@naver.com>
2024-03-19 01:57:13 +0900

    feat: home페이지 QuickMenu 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BottomNav from "@/components/BottomNav";
+import QuickMenu from "@/components/QuickMenu";
 import TopNav from "@/components/TopNav";
 
 export default function Home() {
   return (
     <>
       <TopNav />
+      <QuickMenu />
       <BottomNav />
     </>
   );
---
+++ src/components/QuickMenu.tsx
@@ -0,0 +1,220 @@
+    import Image from "next/image";
+
+    export default function QuickMenu(){
+    return(
+        <>
+            <div className="overflow-auto">
+            <div className="flex">
+                <div data-unit-type="QUICK_ROUND_SCROLL" >
+                    <div className="flex overflow-x-auto">
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a  href="https://m.ssg.com/page/dvstore_emart.ssg?mGnbYn=N">
+                                <div >
+                                    <Image alt="쓱배송" className="rounded-full " src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509051067186177991717_407.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span >쓱배송</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a  href="https://m.ssg.com/page/dvstore_morning.ssg?mGnbYn=N">
+                                <div >
+                                    <Image alt="새벽배송" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509053385538505647850_845.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span >새벽배송</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a  href="https://m.ssg.com/page/dvstore_oneday.ssg?mGnbYn=N">
+                                <div >
+                                    <Image alt="쓱1DAY배송" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509125498828183756818_909.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span >쓱1DAY<br/>배송</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a  href="https://m.ssg.com/page/dvstore_traders.ssg?mGnbYn=N">
+                                <div >
+                                    <Image alt="트레이더스 쓱배송" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509144825099566803066_188.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span >트레이더스<br/>쓱배송</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a  href="https://m.ssg.com/membership/gate.ssg">
+                                <div >
+                                    <Image alt="유니버스 클럽" className="rounded-full" src="https://sui.ssgcdn.com/cmpt/banner/202401/2024010509151151913264546326_754.webp" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span >유니버스<br/>클럽</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a  href="https://event.ssg.com/m/eventDetail.ssg?nevntId=1000000007897">
+                                <div >
+                                    <Image alt="SSG 상품권" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509161389155903418590_165.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span >SSG 상품권</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="http://m.ssg.com/event/eventMain.ssg">
+                                <div >
+                                    <Image alt="이벤트" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509180257727350217735_496.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span>이벤트</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/page/SpecialPrice.ssg">
+                                <div>
+                                    <Image alt="특가/세일" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509182223765868711686_213.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span>특가/세일</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/service/ssgtv/main.ssg?dispVodcBrocId=9999">
+                                <div>
+                                    <Image alt="쓱라이브" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509185927813904386390_967.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span>쓱라이브</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/page/farmersmarket.ssg">
+                                <div>
+                                    <Image alt="신선직송" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202402/2024022117311895465343844634_14.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span>신선직송</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="http://m.ssg.com/express/freqbuy.ssg">
+                                <div>
+                                    <Image alt="자주구매" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509191930136537917653_371.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span>자주구매</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/service/gift/main.ssg">
+                                <div>
+                                    <Image alt="선물하기" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202401/2024010509195916439037718903_410.png&amp;w=128&amp;h=128" loading="lazy" width={64} height={64}/>
+                                </div>
+                                <span>선물하기</span>
+                            </a>
+                        </div>
+                    </div>
+                    <div className="flex">
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m-shinsegaemall.ssg.com/page/dept/home.ssg?ldt=menu&amp;mccode=6009">
+                                <div>
+                                    <Image alt="백화점" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202306/2023062211334531279051274015_740.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>백화점</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://si.mfamily.ssg.com/">
+                                <div>
+                                    <Image alt="신규 카테고리 메뉴" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202403/2024031313232892407800438780_352.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>S.I. Village</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/page/ssgfoodmarket/main.ssg">
+                                <div>
+                                    <Image alt="프리미엄식품관" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202306/2023062011223156401848383184_744.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>프리미엄<br/>식품관</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/page/ssg_luxury/main/_v39.ssg">
+                                <div>
+                                    <Image alt="명품" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202312/2023121216093071049155376915_523.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>명품</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/page/mondaymoon/main/_v21.ssg">
+                                <div>
+                                    <Image alt="뷰티" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202312/2023123102014887905819645581_480.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96} />
+                                </div>
+                                <span>뷰티</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="http://m.ssg.com/page/ssgfashion/_v19.ssg">
+                                <div>
+                                    <Image alt="패션" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202307/2023072716560129162777030377_717.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>패션</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://wconcept.mfamily.ssg.com/">
+                                <div>
+                                    <Image alt="w컨셉" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202312/2023120515471045888908853990_624.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>W컨셉</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m.ssg.com/page/golf.ssg">
+                                <div>
+                                    <Image alt="골프" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202311/2023112314542464485872186687_117.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>골프</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m-triip.ssg.com/hotel/main.ssg?ldt=menu&amp;mccode=7013">
+                                <div>
+                                    <Image alt="여행" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202308/2023083016383490602411998241_586.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>여행</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://starbucks.mfamily.ssg.com">
+                                <div>
+                                    <Image alt="스타벅스" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202308/2023083016365154671009162400_652.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>스타벅스</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://premiumoutlets.mfamily.ssg.com/">
+                                <div>
+                                    <Image alt="프리미엄아울렛" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202308/2023083016392184355252543625_39.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>프리미엄<br/>아울렛</span>
+                            </a>
+                        </div>
+                        <div className="text-center text-[13px] mr-[12px] font-[Pretendard-Thin]">
+                            <a href="https://m-shinsegaemall.ssg.com/specialStore/salvatoreferragamo/main.ssg">
+                                <div>
+                                    <Image alt="페라가모" className="rounded-full" src="https://simg.ssgcdn.com/trans.ssg?src=/cmpt/banner/202308/2023083016393675468768254976_98.png&amp;w=128&amp;h=192" loading="lazy" width={64} height={96}/>
+                                </div>
+                                <span>페라가모</span>
+                            </a>
+                        </div>
+                        </div>
+                        </div>
+                        <div >
+                            <div >
+                                <div aria-valuemax={100} aria-valuemin={0} aria-valuenow={4.210526161348289} role="progressbar" aria-label="배너 스크롤 진행율" style={{width: "4.21053%"}}>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div >
+                        <div >
+                        </div>
+                    </div>
+                </div>
+        </>
+    )
+    }
