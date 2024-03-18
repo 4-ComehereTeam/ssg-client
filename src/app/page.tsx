da066b9e64322f4f40dc0639fafb4fa98c05adbe
sw-devr <sw4689@naver.com>
2024-03-19 01:36:22 +0900

    feat: BottonNav 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import BottomNav from "@/components/BottomNav";
 import TopNav from "@/components/TopNav";
 
 export default function Home() {
   return (
     <>
       <TopNav />
+      <BottomNav />
     </>
   );
 }
