ab47bebccdaf73ce4746af909595770e73db0573
ddolboghi <rhdfyd128@gmail.com>
2024-03-18 18:57:22 +0900

    fix: 체크박스 크기 고정되도록 수정

--- src/components/ui/Checkbox.tsx
+++ src/components/ui/Checkbox.tsx
@@ -14,26 +14,26 @@ export default function Checkbox({
   checkboxShape = "rounded-full",
 }: CheckboxProps) {
   return (
-    <div className="basis-3/4 flex flex-row gap-2">
+    <div className="basis-3/4 flex flex-row leading-4">
       <input
         id={id}
         type="checkbox"
         onChange={onChange}
         checked={checked}
         className={`
+        flex-none w-[17px] h-[17px]
         appearance-none
-        w-[17px] h-[17px]
         border border-gray-300 ${checkboxShape} 
-        bg-[url('/assets/images/check.svg')]
         bg-no-repeat
         bg-center
         checked:bg-[#FE5B5B] focus:outline-none
         checked:bg-no-repeat
         checked:bg-center
         checked:bg-[url('/asset/images/check.svg')]
+        disabled: bg-[url('/asset/images/check.svg')]
         `}
       />
-      <label htmlFor={id} className="basis-3/4">
+      <label htmlFor={id} className=" pl-2">
         {text}
       </label>
     </div>
