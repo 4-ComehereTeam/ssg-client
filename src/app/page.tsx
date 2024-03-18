232c0f5510ec17d631a8213747d2243fb494ea93
sw-devr <sw4689@naver.com>
2024-03-19 02:26:25 +0900

    feat: home페이지 SearchBar 컴포넌트 추가 및 layout 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import BottomNav from "@/components/BottomNav";
 import QuickMenu from "@/components/QuickMenu";
+import SearchBar from "@/components/SearchBar";
 import TopNav from "@/components/TopNav";
 
 export default function Home() {
   return (
     <>
+      <SearchBar />
       <TopNav />
       <QuickMenu />
       <BottomNav />
