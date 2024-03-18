06422c6641becc468aa60ddb451d04bffc058da3
ddolboghi <rhdfyd128@gmail.com>
2024-03-18 11:28:15 +0900

    feat: 회원가입 동의 페이지 구현

---
+++ src/app/member/signup/agree/page.tsx
@@ -0,0 +1,10 @@
+import SignupAgreeForm from "@/components/form/SignupAgreeForm"
+import React from "react"
+
+export default function SignupAgreePage() {
+  return (
+    <div>
+      <SignupAgreeForm />
+    </div>
+  )
+}
