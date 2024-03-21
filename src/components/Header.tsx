ff3a776dda6b464cbee2276eb2e28837dd3b57fb
ddolboghi <rhdfyd128@gmail.com>
2024-03-21 17:03:32 +0900

    fix: 뒤로가기 이미지 너비, 높이 부여

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,13 @@ function Header() {
   return (
     <div className="flex justify-between pr-[49px] w-full h-[42px] text-sm text-center text-black whitespace-nowrap border-b border-solid border-stone-300">
       <Link href="#" className="w-[50px] flex item-center justify-center">
-        <Image loading="lazy" src={backArrow} alt="뒤로가기" />
+        <Image
+          loading="lazy"
+          src={backArrow}
+          width={22}
+          height={41}
+          alt="뒤로가기"
+        />
       </Link>
       <div className="flex-auto my-auto">로그인</div>
     </div>
