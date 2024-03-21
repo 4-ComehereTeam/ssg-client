e48a5cf572c4ed96e5534e139216c02c43c4590b
ddolboghi <rhdfyd128@gmail.com>
2024-03-21 16:57:15 +0900

    feat: 동의 타입 추가

---
+++ src/data/agreements.ts
@@ -0,0 +1,46 @@
+export const ssgPointMktAgreements = [
+  {
+    num: 0,
+    id: "ssgPointMktAgr1",
+    text: "(선택) 마케팅 정보 제공을 위한 개인정보 수집 및 이용 동의",
+  },
+  {
+    num: 1,
+    id: "ssgPointMktAgr2",
+    text: "(선택) 선택 정보 이마트/신세계백화점 공동 개인정보 수집 및 이용 동의",
+  },
+]
+
+export const ssgcomMktAgreements = [
+  {
+    num: 0,
+    id: "ssgcomMktAgr1",
+    text: "(선택) 마케팅 정보 제공을 위한 개인정보 수집 및 이용 동의",
+  },
+]
+
+export const ssgPointMktReceiveMethods = [
+  { num: 0, id: "ssgPointEmail", text: "이메일" },
+  { num: 1, id: "ssgPointSms", text: "문자" },
+  { num: 2, id: "ssgPointMail", text: "우편물" },
+  { num: 3, id: "ssgPointCall", text: "텔레마케팅" },
+]
+
+export const ssgcomMktReceiveMethods = [
+  { num: 0, id: "ssgcomEmail", text: "이메일" },
+  { num: 1, id: "ssgcomSms", text: "문자" },
+]
+
+export const ssgPointMktReceiveNotice =
+  "마케팅 정보 수신 동의를 하시면 신세계포인트 서비스 및 이벤트 정보를 받으실 수 있습니다."
+
+export const ssgcomMktReceiveNotice =
+  "마케팅 정보 수신 동의를 하시면 SSG.COM 서비스 및 이벤트 정보를 받으실 수 있습니다."
+
+export const certificationAgreements = [
+  { num: 0, id: "agree1", text: "개인정보 이용 및 제공 동의" },
+  { num: 1, id: "agree2", text: "통신사 이용약관 동의" },
+  { num: 2, id: "agree3", text: "고유식별정보 처리 동의" },
+  { num: 3, id: "agree4", text: "서비스 이용약관 동의" },
+  { num: 4, id: "agreeAll", text: "전체 동의" },
+]
