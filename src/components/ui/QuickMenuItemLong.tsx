50635a1c1f2ff144b9de7e10a64b37e2afcfa176
sw-devr <sw4689@naver.com>
2024-03-22 09:33:14 +0900

    feat: QuickMenuItem 컴포넌트 분리

---
+++ src/components/ui/QuickMenuItemLong.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+interface QuickMenuItemProp {
+    name : string,
+    path : string,
+    src : string
+}
+
+export default function QuickMenuItemLong({name, path, src}:QuickMenuItemProp){
+    return(
+        <>
+            <div className=" text-center text-[13px] mr-[12px] font-[Pretendard-Thin] W-[64px]">
+                <a  href={path} className="w-[64px] block ">
+                    <div className="w-[64px]">
+                        <Image alt={name} className="rounded-full " src={src} loading="lazy" width={64} height={96}/>
+                    </div>
+                    <span >{name}</span>
+                </a>
+            </div>
+        </>
+    )
+}
