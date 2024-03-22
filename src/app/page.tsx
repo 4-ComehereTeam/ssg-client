c659f5f2f71d577a4bebc5904a8d51a4efa9712a
sw-devr <sw4689@naver.com>
2024-03-22 13:32:59 +0900

    feat: Swiper 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import QuickMenu from "@/components/QuickMenu";
 import SearchBar from "@/components/SearchBar";
 import TopNav from "@/components/TopNav";
 import AdBox from "@/components/ui/AdBox";
+import Swiper from "@/components/ui/Swiper";
 
 export default function Home() {
   return (
     <>
       <SearchBar />
       <TopNav />
+      <Swiper />
       <QuickMenu />
       <AdBox />
       <BottomNav />
