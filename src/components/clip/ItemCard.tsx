e9badfd1e6f19bbf0873b85ae2930db1f4be13b5
ddolboghi <rhdfyd128@gmail.com>
2024-03-22 13:30:41 +0900

    feat: 좋아요 페이지 생성

--- src/components/clip/ItemCard.tsx
+++ src/components/clip/ItemCard.tsx
@@ -2,66 +2,119 @@ import Image from "next/image"
 import * as React from "react"
 
 interface ItemCardProps {
-  imageUrl?: string
-  iconUrl?: string
-  badgeNumber?: string
-  productImageUrl?: string
-  productName?: string
-  originalPrice?: string
-  discountPercentage?: number
-  discountedPrice?: string
-  rating?: number
-  totalReviews?: string
-  isGuaranteedFresh?: boolean
+  thumbnailUrl: string
+  alt: string
+  itemName: string
+  brand: string
+  price: number
+  //아래는 없어도 0으로 받음
+  discountRate: number
+  star: number
+  totalReviews: number
+  cardWidth?: string
+  cardHeight?: string
 }
 
 export default function ItemCard({
-  imageUrl,
-  iconUrl,
-  badgeNumber,
-  productImageUrl,
-  productName,
-  originalPrice,
-  discountPercentage,
-  discountedPrice,
-  rating,
+  thumbnailUrl,
+  alt,
+  itemName,
+  brand,
+  price,
+  discountRate,
+  star,
   totalReviews,
-  isGuaranteedFresh,
+  cardWidth = "w-36",
+  cardHeight = "h-36",
 }: ItemCardProps) {
+  const discountPrice =
+    discountRate !== 0 ? price * ((100 - discountRate) / 100) : price
+  const finalPrice = new Intl.NumberFormat().format(Math.round(discountPrice))
+  const originalPrice = new Intl.NumberFormat().format(price)
   return (
-    <article className="flex flex-col w-[25%]">
-      <div className="flex overflow-hidden relative flex-col gap-0 items-start pb-20 pl-20 text-xs leading-3 text-center whitespace-nowrap aspect-square">
-        {/* <Image src={imageUrl} alt="썸네일" width={300} /> */}
-        <div>하트, 장바구니</div>
-        <div className="relative justify-center items-center w-5 h-5 bg-stone-500">
-          {badgeNumber}
+    <div className={`flex flex-col pt-2 pb-5 ${cardWidth}`}>
+      {/* <Image src={thumbnailUrl} alt={alt} width={cardWidth} /> */}
+      <div className={`bg-violet-400 ${cardWidth} ${cardHeight}`}></div>
+      <div className="flex flex-row justify-end w-full items-center">
+        <button className="w-[28px] h-[28px]">
+          <svg
+            width="20px"
+            viewBox="0 0 24 24"
+            focusable="false"
+            name="LikeIcon"
+          >
+            <path
+              d="M12 21.288L4.06802 12.72C2.94002 11.496 2.40002 10.224 2.40002 8.84398C2.40002 5.95198 4.69202 3.59998 7.50002 3.59998C9.44402 3.59998 11.136 4.72798 12 6.38398C12.864 4.72798 14.556 3.59998 16.5 3.59998C19.308 3.59998 21.6 5.95198 21.6 8.84398C21.6 10.224 21.06 11.484 19.944 12.708L12 21.288ZM7.50002 4.79998C5.35202 4.79998 3.60002 6.61198 3.60002 8.84398C3.60002 9.92398 4.03202 10.896 4.94402 11.904L12 19.512L19.056 11.904C19.968 10.896 20.4 9.92398 20.4 8.84398C20.4 6.61198 18.648 4.79998 16.5 4.79998C14.352 4.79998 12.6 6.61198 12.6 8.84398H11.4C11.4 6.61198 9.64802 4.79998 7.50002 4.79998Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </button>
+        <button className="w-[28px] h-[28px]">
+          <svg
+            width="20px"
+            viewBox="0 0 24 24"
+            focusable="false"
+            name="CartIcon"
+          >
+            <rect
+              x="6"
+              y="8.40002"
+              width="14.4"
+              height="1.2"
+              fill="currentColor"
+            ></rect>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M6 19.2C6 20.52 7.08 21.6 8.4 21.6C9.72 21.6 10.8 20.52 10.8 19.2C10.8 17.88 9.72 16.8 8.4 16.8C7.08 16.8 6 17.88 6 19.2ZM7.20004 19.2C7.20004 18.48 7.68004 18 8.40004 18C9.12004 18 9.60004 18.48 9.60004 19.2C9.60004 19.92 9.12004 20.4 8.40004 20.4C7.68004 20.4 7.20004 19.92 7.20004 19.2Z"
+              fill="currentColor"
+            ></path>
+            <path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M15.6 19.2C15.6 20.52 16.68 21.6 18 21.6C19.32 21.6 20.4 20.52 20.4 19.2C20.4 17.88 19.32 16.8 18 16.8C16.68 16.8 15.6 17.88 15.6 19.2ZM16.8001 19.2C16.8001 18.48 17.2801 18 18.0001 18C18.7201 18 19.2001 18.48 19.2001 19.2C19.2001 19.92 18.7201 20.4 18.0001 20.4C17.2801 20.4 16.8001 19.92 16.8001 19.2Z"
+              fill="currentColor"
+            ></path>
+            <path
+              d="M19.08 15.6H7.32001L4.08001 4.79998H1.20001V3.59998H5.04001L8.28001 14.4H18.12L20.4 7.07998L21.6 7.31998L19.08 15.6Z"
+              fill="currentColor"
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <div>
+        <p className="text-xs">
+          <span className="font-extrabold">{brand} </span>
+          {itemName}원
+        </p>
+      </div>
+      <div>
+        {discountRate !== 0 && (
+          <span className="text-xs line-through">{originalPrice}</span>
+        )}
+        <div className="text-base font-bold">
+          <span className="text-[#FF5452]">{discountRate}%</span>
+          <span className="ml-1">{finalPrice}원</span>
         </div>
+        {star && totalReviews && (
+          <div className="flex flex-row gap-1 text-xs">
+            <svg
+              width={11}
+              viewBox="0 0 11 16"
+              focusable="false"
+              name="StarFill"
+            >
+              <path
+                fill="#CFCFCF"
+                d="m2.089 13 .906-4.073L0 6.205l3.94-.35L5.5 2l1.56 3.856 3.94.349-2.995 2.722L8.911 13 5.5 10.838 2.089 13Z"
+              ></path>
+            </svg>
+            <span className="text-[#777777]">{star}</span>
+            <div className="bg-[#E5E5E5] border w-0 h-[12px] mt-[3px]"></div>
+            <span>{totalReviews}건</span>
+          </div>
+        )}
       </div>
-
-      <h3 className="mt-3.5 w-full text-sm text-neutral-800">{productName}</h3>
-      <p className="mt-1.5 w-full text-xs text-neutral-400">{originalPrice}</p>
-      <div className="mt-1.5 w-full text-base font-semibold text-black">
-        {discountPercentage}%{" "}
-        <span className="text-black">{discountedPrice}</span>
-      </div>
-      <div className="flex gap-1 mt-1.5 text-xs whitespace-nowrap text-neutral-500">
-        <Image
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/e67fd611d92cd49cc52905d0d88f731619d46447529ac185b379343e81e8b2e0?apiKey=728ab64874b9457aabdcf41866c4ead0&"
-          alt=""
-          className="shrink-0 aspect-[0.79] w-[11px]"
-        />
-        <p className="my-auto">{rating}</p>
-        <div
-          className="shrink-0 self-start w-px bg-zinc-300 h-[11px]"
-          aria-hidden="true"
-        />
-        <p>{totalReviews}</p>
-      </div>
-      {isGuaranteedFresh && (
-        <div className="justify-center px-1.5 py-1.5 mt-1.5 text-xs whitespace-nowrap bg-neutral-100 text-neutral-700">
-          신선보장
-        </div>
-      )}
-    </article>
+    </div>
   )
 }
