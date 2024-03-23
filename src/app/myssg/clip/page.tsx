b0fc35f67afcd277aadb9768996a6fe9fd5ac181
ddolboghi <rhdfyd128@gmail.com>
2024-03-23 17:47:27 +0900

    fix: 데이터 페칭 위한 로직 수정

--- src/app/myssg/clip/page.tsx
+++ src/app/myssg/clip/page.tsx
@@ -3,23 +3,13 @@ import ClipItems from "@/components/clip/ClipItems"
 import ClipNavbar from "@/components/clip/ClipNavbar"
 import React from "react"
 
-//좋아요 개수 임시 데이터
-const clipNums = {
-  item: 2,
-  brand: 0,
-  category: 0,
-}
-
-//상품 목록 임시 데이터
-const clipItems = [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]
-
 export default function ClipPage() {
   //Header 추가하기
   return (
     <>
       <ClipHeader />
-      <ClipNavbar clipNums={clipNums} />
-      <ClipItems clipItems={clipItems} />
+      <ClipNavbar />
+      <ClipItems />
     </>
   )
 }
