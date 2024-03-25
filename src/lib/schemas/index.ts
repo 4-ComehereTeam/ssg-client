de6d2b1c6b0cc65d4ce21c496c5eda26cde80b15
ddolboghi <rhdfyd128@gmail.com>
2024-03-25 12:20:16 +0900

    feat: 검증 항목 추가

--- src/lib/schemas/index.ts
+++ src/lib/schemas/index.ts
@@ -12,6 +12,15 @@ export const SignupSchema = z.object({
       "아이디는 영어 또는 숫자로 6~20자리여야 합니다.",
     )
     .trim(),
+
+  checkId: z.string().refine((value) => value === "1", {
+    message: "아이디 중복확인을 해주세요.",
+  }),
+
+  isDuplId: z.string().refine((value) => value === "0", {
+    message: "중복된 아이디입니다.",
+  }),
+
   password: z
     .string()
     .regex(
@@ -26,13 +35,12 @@ export const SignupSchema = z.object({
   //TODO: 주소 입력 폼 받으면 zipcode, address, detailAddress로 나누기
   address: z.string().min(1, "주소는 필수 입력 항목입니다."),
   phone: z
-    .string()
-    .min(11, {
-      message: "휴대폰번호는 필수 입력 항목입니다.",
+    .string({
+      required_error: "휴대폰번호는 필수 입력 항목입니다.",
     })
     .regex(
       /^01([0|1|6|7|8|9])([0-9]{4})([0-9]{4})$/,
-      "휴대폰 번호 형식이 올바르지 않습니다. (예: 010-1234-5678)",
+      "휴대폰번호 형식이 올바르지 않습니다. (예: 010-1234-5678)",
     ),
   email: z.string().email({
     message: "이메일 형식에 맞지 않습니다. (예: user@ssg.com)",
