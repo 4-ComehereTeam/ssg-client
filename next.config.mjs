b7c43e3682cca7e1b942bdc3c9c6927eba24b090
sw-devr <sw4689@naver.com>
2024-03-25 19:51:35 +0900

    fix : BottomNav 아이콘 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images:{
-        domains : ["sui.ssgcdn.com", "simg.ssgcdn.com", "sitem.ssgcdn.com"]
+        domains : ["sui.ssgcdn.com", "simg.ssgcdn.com", "sitem.ssgcdn.com", "img.icons8.com"]
     }
 };
 
