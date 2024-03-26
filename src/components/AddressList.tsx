923aced24aae56ff782f2ad5e17a9eca1648917c
sw-devr <sw4689@naver.com>
2024-03-26 11:34:28 +0900

    feat: AddressList 컴포넌트 추가

---
+++ src/components/AddressList.tsx
@@ -0,0 +1,26 @@
+
+interface addressListProps {
+    addressAliase : string
+    name : string
+    address : string
+    phone : string
+}
+
+export default function AddressList({addressAliase, name, address, phone}:addressListProps){
+    return(
+
+        <div className='m-2 border-2 border-black p-4'>
+        <div className='flex items-center'>
+            <input type='radio' className='accent-red-500 w-[18px] h-[18px]' name="addressList"/> 
+            <span className='font-bold pl-2'>{addressAliase}</span>
+        </div>
+
+        <div>
+            <h3 className='text-[14px] mt-2'>{address}</h3>
+        </div>
+        <div>
+            <h3 className='text-[12px] mt-1 text-[#888888]'>{name} / {phone}</h3>
+        </div>
+    </div>
+    )
+}
--- src/components/SelectAddressModal.tsx
+++ src/components/SelectAddressModal.tsx
@@ -1,7 +1,6 @@
-// import X_icon from '@/images/X_icon.png'
 import Image from 'next/image'
 import Link from 'next/link'
-// import AddressList from './AddressList'
+import AddressList from './AddressList'
 
 interface AddressModalProps {
     modalOpen: boolean
@@ -12,19 +11,20 @@ function SelectAddressModal({ modalOpen, setModalOpen }: AddressModalProps) {
         <div>
             <div className="bg-black/60 absolute inset-0 z-50">
                 <div className="fixed inset-x-0 top-0 bottom-0 flex flex-col border  bg-white">
-                    <header
-                        className="h-[45px] flex items-center border-b-[1px] border-[rgba(0, 0, 0, 0.07)] sticky top-0 bg-white z-50"
+
+                <header
+                    className="h-[45px] flex items-center border-b-[1px] border-[rgba(0, 0, 0, 0.07)] sticky top-0 bg-white z-50"
+                >
+                    <button
+                        className="w-[50px] h-full"
+                        onClick={() => {
+                            setModalOpen(false)
+                        }}
                     >
-                        <button
-                            className="w-[50px] h-full"
-                            onClick={() => {
-                                setModalOpen(false)
-                            }}
-                        >
-                            <Image width="24" height="22" className='mx-auto' src="https://img.icons8.com/ios/50/left--v1.png" alt="backButton"/>
-                        </button>
-                        <h3 className="text-[14px] w-full text-center mx-auto">배송지 선택</h3>
-                    </header>
+                        <Image width="24" height="22" className='mx-auto' src="https://img.icons8.com/ios/50/left--v1.png" alt="backButton"/>
+                    </button>
+                    <h3 className="text-[14px] w-full text-center mx-auto">배송지 선택</h3>
+                </header>
 
                     <div className='pt-[40px] px-[16px] mb-[16px]'>
                         <h1 className='text-[24px] tracking-[-0.3px] font-bold'>어디로 보내드릴까요?</h1>
@@ -34,21 +34,15 @@ function SelectAddressModal({ modalOpen, setModalOpen }: AddressModalProps) {
                         + 신규배송지등록
                     </Link>
 
-                    <div className='m-2 border-2 border-black p-4'>
-                        <div className='flex items-center'>
-                            <input type='radio' className='accent-red-500 w-[18px] h-[18px]'/> 
-                            <span className='font-bold pl-2'>{"홍길동"}</span>
-                        </div>
+                    <AddressList addressAliase='집' name='홍길동' address='[555555] 부산광역시 해운대구 센텀리더스마크 4층' phone='010-0000-0000' />
+                    <AddressList addressAliase='회사' name='홍길동' address='[555555] 부산광역시 해운대구 센텀리더스마크 4층' phone='010-0000-0000' />
+                    <AddressList addressAliase='학교' name='홍길동' address='[555555] 부산광역시 해운대구 센텀리더스마크 4층' phone='010-0000-0000' />
 
-                        <div>
-                            <h3 className='text-[14px] mt-2'>[555555] 부산광역시 해운대구 센텀리더스마크 4층</h3>
-                        </div>
-                        <div>
-                            <h3 className='text-[12px] mt-1 text-[#888888]'>{"홍길동"} / {"010-0000-0000"}</h3>
-                        </div>
-                    </div>
-
-                    <button className='bottom-0 left-0 right-0 fixed h-14 z-50 bg-[#ff5452] text-white font-semibold'>
+                    <button 
+                    className='bottom-0 left-0 right-0 fixed h-14 z-50 bg-[#ff5452] text-white font-semibold'
+                    onClick={() => {
+                        setModalOpen(false)
+                    }}>
                         변경하기
                     </button>
 
