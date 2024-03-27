dbc0a9dae6fb1b7d0cc6faaf4f2cb73871ceef3c
sw-devr <sw4689@naver.com>
2024-03-27 09:49:19 +0900

    fix: 결제방법 아이콘 변경

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -76,7 +76,7 @@ export default function OrderPage(){
                     </div>
                 </div>
 
-                <div className={payList ? "bg-white m-4 p-2 rounded-xl h-72" : "bg-white m-4 p-2 rounded-xl"}>
+                <div className={payList ? "bg-white m-4 p-2 rounded-xl h-96" : "bg-white m-4 p-2 rounded-xl"}>
                     <div className=" pb-2">
                         <span className="text-[18px] font-semibold my-5 mx-2">
                             결제방법
@@ -207,12 +207,12 @@ export default function OrderPage(){
                     </div>
                     <div className="flex px-[16px] py-[15px]">
                         <div className="flex justify-between">
-                            <Image src="https://sitem.ssgcdn.com/74/87/78/item/1000531788774_i1_140.jpg" alt="한우" width={150} height={150}/>
+                            <Image src={"https://sitem.ssgcdn.com/74/87/78/item/1000531788774_i1_140.jpg"} alt="한우" width={150} height={150}/>
                         </div>
                         <div className="flex flex-col justify-between text-xs mx-2">
                             <div>
                                 <span>{"이마트몰"}</span>
-                                <span className="text-[#666666]">{"· 주식회사 태성축산유통"}</span>
+                                <span className="text-[#666666]">{" 주식회사 태성축산유통"}</span>
                             </div>
                             <div>
                                 <span className="font-extrabold">{"구미우"}</span>
@@ -247,7 +247,7 @@ export default function OrderPage(){
 function PayList(){
     return(
         <>
-        <div className="h-50 bg-white w-full my-5 text-[14px]">
+        <div className="h-60 bg-white w-full my-5 text-[14px]">
             <ul>
                 <div className="w-full flex">
                     <li className="m-1 flex-1 inline-block float-left ">
@@ -256,8 +256,9 @@ function PayList(){
                     </li>
                     <li className="m-1 flex-1 inline-block float-left">
                         <input type="radio" id="select2" name="paying" className="hidden peer"/>
-                        <label htmlFor = "select2" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">
-                            SSGPAY
+                        <label htmlFor = "select2" className="w-full relative justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">
+                            SSG<span className="text-red-500">PAY.</span>
+                            <span className="absolute text-[8px] bg-[#d8d8d8] px-1 top-0 left-0">APP</span>
                         </label>
                     </li>
                     <li className="m-1 flex-1 inline-block float-left">
@@ -268,25 +269,37 @@ function PayList(){
                 <div className="w-full flex">
                     <li className="m-1 flex-1 inline-block float-left">
                         <input type="radio" id="select4" name="paying" className="hidden peer"/>
-                        <label htmlFor = "select4" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">PAYCO</label>
+                        <label htmlFor = "select4" className="w-full justify-center text-red-700 font-bold items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[15px] peer-checked:border-0">
+                            PAYCO</label>
                     </li>
                     <li className="m-1 flex-1 inline-block float-left">
                         <input type="radio" id="select5" name="paying" className="hidden peer"/>
-                        <label htmlFor = "select5" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">SAMSUNG Pay</label>
+                        <label htmlFor = "select5" className="w-full justify-center text-blue-900 font-bold items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">
+                            SAMSUNG <br/>Pay</label>
                     </li>
                     <li className="m-1 flex-1 inline-block float-left">
                         <input type="radio" id="select6" name="paying" className="hidden peer"/>
                         <label htmlFor = "select6" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">휴대폰 소액결제</label>
                     </li>
                 </div>
-                <div className="w-2/3 flex">
+                <div className="w-full flex">
                     <li className="m-1 flex-1 inline-block float-left">
                         <input type="radio" id="select7" name="paying" className="hidden peer"/>
-                        <label htmlFor = "select7" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">실시간 계좌이체</label>
+                        <label htmlFor = "select7" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">무통장 입금</label>
                     </li>
                     <li className="m-1 flex-1 inline-block float-left">
                         <input type="radio" id="select8" name="paying" className="hidden peer"/>
-                        <label htmlFor = "select8" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">해외발급 신용카드</label>
+                        <label htmlFor = "select8" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">실시간 계좌이체</label>
+                    </li>
+                    <li className="m-1 flex-1 inline-block float-left">
+                        <input type="radio" id="select9" name="paying" className="hidden peer"/>
+                        <label htmlFor = "select9" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">해외발급 신용카드</label>
+                    </li>
+                </div>
+                <div className="w-1/3 flex">
+                    <li className="m-1 flex-1 inline-block float-left">
+                        <input type="radio" id="select10" name="paying" className="hidden peer"/>
+                        <label htmlFor = "select10" className="w-full justify-center items-center text-center min-h-[50px] flex bg-white border-[1px] peer-checked:bg-black peer-checked:text-white peer-checked:font-bold text-[13px] peer-checked:border-0">Alipay</label>
                     </li>
                 </div>
             </ul>
