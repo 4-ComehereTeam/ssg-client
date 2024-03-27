30e301cf257f20b8791f88c94822e22a66cad3d9
sw-devr <sw4689@naver.com>
2024-03-28 02:05:50 +0900

    feat: 약관보기 modal 추가

--- src/app/order/complete/page.tsx
+++ src/app/order/complete/page.tsx
@@ -16,7 +16,7 @@ export default function Complete(){
     return(
         <>  
         <div className="flex justify-between w-full h-[45px] text-sm text-center text-black whitespace-nowrap border-b border-solid border-stone-300 sticky top-0 z-50 bg-white shadow-sm">
-            <div className="flex-auto my-auto">주문완료</div>
+            <div className="flex justify-center items-center relative left-[25px] w-full ">주문완료</div>
             <Link href="/" className="w-[50px] flex item-center justify-center">
                 <Image
                 loading="lazy"
