9cd61195e099e5c7bbd946a1e03807345a1ceb2e
ddolboghi <rhdfyd128@gmail.com>
2024-03-28 15:14:39 +0900

    chore: nextjs 환경 설정 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images:{
-        domains : ["sui.ssgcdn.com", "simg.ssgcdn.com", "sitem.ssgcdn.com", "img.icons8.com"]
-    }
-};
+  reactStrictMode: false,
+  images: {
+    domains: [
+      "sui.ssgcdn.com",
+      "simg.ssgcdn.com",
+      "sitem.ssgcdn.com",
+      "img.icons8.com",
+    ],
+  },
+  env: {
+    API_BASE_URL: process.env.API_BASE_URL,
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    KAKAO_CLIENT_ID: process.env.KAKAO_CLIENT_ID,
+    KAKAO_CLIENT_SECRET: process.env.KAKAO_CLIENT_SECRET,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
