141e1b881297f91c6f02eca9f6869b34335c0e92
sw-devr <sw4689@naver.com>
2024-03-28 15:06:58 +0900

    feat: cart페이지, address페이지 header추가

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,10 +1,12 @@
 import CartList from '@/components/cart/CartList'
 import DeliveryAddress from '@/components/cart/DeliveryAddress'
 import ChangeAddress from '@/components/cart/ChangeAddress'
+import HeaderOfCart from '@/components/ui/Headers/HeaderOfCart'
 function Cart() {
     return (
         <main>
             {/* <HeaderTitle title="장바구니" /> */}
+            <HeaderOfCart title='장바구니'/>
             <DeliveryAddress />
             <ChangeAddress />
             <CartList />
