afba7de43b9b96e3f72e2889b6bd2f7d3d12c638
ddolboghi <rhdfyd128@gmail.com>
2024-03-31 02:08:14 +0900

    fix: next-auth 로그인 로직 수정

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth"
 import CredentialsProvider from "next-auth/providers/credentials"
+import NaverProvider from "next-auth/providers/naver"
 import KakaoProvider from "next-auth/providers/kakao"
 import GoogleProvider from "next-auth/providers/google"
 
@@ -15,72 +16,80 @@ export const options: NextAuthOptions = {
         if (!credentials?.signinId || !credentials?.password) {
           return null
         }
-        console.log("credentials:", credentials)
+        // console.log("credentials:", credentials)
 
         /**
          * 백엔드에서 받은 응답에 token있어야함
          * user를 리턴하면 callbacks의 signIn에서 user로 전달되고
          * 그 다음 jwt콜백의 user로 전달됨
          */
-        const res = await fetch(`${process.env.API_BASE_URL}/members/signIn`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            signInId: credentials.signinId,
-            password: credentials.password,
-          }),
-        })
-        console.log("signin api res status:", res.status)
-        if (res.ok) {
-          const member = await res.json()
-          console.log(member)
-          return member.result
-        }
+        try {
+          const res = await fetch(`${process.env.API_BASE_URL}/auth/signIn`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              signinId: credentials.signinId,
+              password: credentials.password,
+            }),
+          })
+          console.log("signin api res status:", res.status)
+          if (res.ok) {
+            const member = await res.json()
+            return member.result
+          }
 
-        return null
+          return null
+        } catch (error) {
+          console.log("signin error:", error)
+          return null
+        }
       },
     }),
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID || "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    NaverProvider({
+      clientId: process.env.NAVER_CLIENT_ID || "",
+      clientSecret: process.env.NAVER_CLIENT_SECRET || "",
     }),
     KakaoProvider({
       clientId: process.env.KAKAO_CLIENT_ID || "",
       clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
   ],
   callbacks: {
     //signIn: authorize후 추가 확인
-    async signIn({ user, profile }) {
-      console.log("signIn user:", user)
-      console.log("signIn profile:", profile)
-      // if (user) {
-      //   /**
-      //    * signIn에서 추가적으로 할만한게 없음
-      //    * OAuth 로그인시 기존 회원인지 아닌지 확인해서
-      //    * 기존 회원이면 credential 방식으로 로그인 시키기?
-      //    * 일단 이메일로 기존 회원 여부만 체크
-      //    * 도 이미 authorize의 백엔드 로그인 요청에서 걸러짐
-      //    */
-      //   const res = await fetch(
-      //     `${process.env.API_BASE_URL}/members/email/check`,
-      //     {
-      //       method: "POST",
-      //       headers: {
-      //         "Content-Type": "application/json",
-      //       },
-      //       body: JSON.stringify({
-      //         email: user.email,
-      //       }),
-      //     },
-      //   )
-      //   if (res.ok) {
-      //     const data = await res.json()
-      //     console.log(data)
-      //   }
-      // }
+    async signIn({ user, profile, account }) {
+      console.log("user:", user.id)
+      console.log("account:", account)
+      // 간편 로그인용 accessToken 발급 받기
+      // credentials는 authorize에서 발급 받았으니 바로 통과
+      if (account?.provider !== "credentials") {
+        //TODO: 백엔드 통신 테스트하기
+        //   try {
+        //     const res = await fetch(`${process.env.API_BASE_URL}/auth/socialSignIn`, {
+        //       method: "GET",
+        //       headers: {
+        //         "Content-Type": "application/json",
+        //         "uuid": user.id
+        //       },
+        //     })
+        //     console.log("OAuth res status:", res.status)
+        //     if (res.ok) {
+        //       const member = await res.json()
+        //       return member.result
+        //     }
+
+        //     return "/member/signin"
+        //   } catch (error) {
+        //     console.log("OAuth error:", error)
+        //     return "/member/signin"
+        //   }
+        return true
+      }
 
       return true
     },
@@ -93,12 +102,9 @@ export const options: NextAuthOptions = {
        * session.user.accessToken 접근 가능
        */
       session.user = token
-      console.log("session: ", session)
       return session
     },
     async jwt({ token, user }) {
-      console.log("jwt token:", token)
-      console.log("jwt user:", user)
       /**
        * authorize에서 return한 user에 들어있는 access_token을
        * session에 저장하기 위해 동일 선상에 추가해서 return
@@ -111,6 +117,5 @@ export const options: NextAuthOptions = {
   },
   pages: {
     signIn: "/member/signin",
-    // error: "/auth_error",
   },
 }
