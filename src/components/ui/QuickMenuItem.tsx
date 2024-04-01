4ad79a598a0ec0d213d38a83e7277b74f3d0cb84
ddolboghi <rhdfyd128@gmail.com>
2024-04-01 17:32:03 +0900

    fix: fregment 태그 삭제

--- src/components/ui/QuickMenuItem.tsx
+++ src/components/ui/QuickMenuItem.tsx
@@ -1,28 +1,33 @@
-import Image from "next/image";
+import Image from "next/image"
 
 interface QuickMenuItemProp {
-    name : string,
-    path : string,
-    src : string
+  name: string
+  path: string
+  src: string
 }
 
 const lineBreak = (text: string) => {
-    return (
-        <span dangerouslySetInnerHTML={{ __html: text.replace(/ /g, '<br />') }} />
-    );
+  return (
+    <span dangerouslySetInnerHTML={{ __html: text.replace(/ /g, "<br />") }} />
+  )
 }
 
-export default function QuickMenuItem({name, path, src}:QuickMenuItemProp){
-    return(
-        <>
-            <div className=" text-center text-xs mr-[12px] font-[Pretendard-Thin] w-[64px]">
-                <a  href={path} className="w-[64px] block ">
-                    <div className="w-[64px]">
-                        <Image alt={name} className="rounded-full " src={src} loading="lazy" width={64} height={64}/>
-                    </div>
-                    <span>{lineBreak(name)}</span>
-                </a>
-            </div>
-        </>
-    )
+export default function QuickMenuItem({ name, path, src }: QuickMenuItemProp) {
+  return (
+    <div className=" text-center text-xs mr-[12px] font-[Pretendard-Thin] w-[64px]">
+      <a href={path} className="w-[64px] block ">
+        <div className="w-[64px]">
+          <Image
+            alt={name}
+            className="rounded-full "
+            src={src}
+            loading="lazy"
+            width={64}
+            height={64}
+          />
+        </div>
+        <span>{lineBreak(name)}</span>
+      </a>
+    </div>
+  )
 }
