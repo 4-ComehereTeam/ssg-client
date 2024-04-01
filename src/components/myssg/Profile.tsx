5e1820eeef62ae4c75804fdf8305d5a1c3839a46
ddolboghi <rhdfyd128@gmail.com>
2024-04-01 20:28:30 +0900

    feat: 사용자 이름 표시

--- src/components/myssg/Profile.tsx
+++ src/components/myssg/Profile.tsx
@@ -1,8 +1,14 @@
+"use client"
+
+import { useSession } from "next-auth/react"
+
 export default function Profile() {
+  const { data: session } = useSession()
+
   return (
     <section className="p-4">
       <div>
-        <h1 className="text-xl">신세계 님</h1>
+        <h1 className="text-xl">{session?.user.name} 님</h1>
         <h2 className="font-extrabold mt-4 text-base">
           SSG에서 즐거운 쇼핑 되세요!
         </h2>
