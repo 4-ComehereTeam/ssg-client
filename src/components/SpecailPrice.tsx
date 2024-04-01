34f56b7c4488ff7ac17ec9d4aa65246e30becc82
sw-devr <sw4689@naver.com>
2024-04-01 20:58:04 +0900

    feat: 특가페이지 추가

---
+++ src/components/SpecailPrice.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import SpecialProduct from '@/components/SpecailProduct'
+
+export default function SpecailPrice() {
+  return (
+    <div>
+      <SpecialProduct
+        name={"첫그릇 장만하기"}
+        brand={"광주요"}
+        subtitle={"~25%할인"}
+        price={30000}
+        id={0}
+        src={'#'}
+        store={null}
+        sale={null}
+        salePrice={null}
+        reviewRating={0}
+        reviewCount={0} />
+    </div>
+  )
+}
