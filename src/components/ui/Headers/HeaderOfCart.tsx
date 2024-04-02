8e701ba388f37ba3625c89a11d6bdd4f24852aef
ddolboghi <rhdfyd128@gmail.com>
2024-04-02 23:06:23 +0900

    fix: 헤더 뒤로가기 아이콘 교체 및 뒤로가기 기능 추가

--- src/components/ui/Headers/HeaderOfCart.tsx
+++ src/components/ui/Headers/HeaderOfCart.tsx
@@ -1,13 +1,17 @@
+"use client"
+
 import Home from "@/asset/images/home.svg"
 import Image from "next/image"
 import Link from "next/link"
+import { useRouter } from "next/navigation"
 
 export default function HeaderOfCart({ title }: { title: string }) {
+  const router = useRouter()
   return (
     <header className="h-[45px] flex items-center border-b-[1px] border-[rgba(0, 0, 0, 0.07)] sticky top-0 shadow-md bg-white z-50">
-      <Link
+      <div
         className="w-[50px] h-full items-center flex justify-center"
-        href={"#"}
+        onClick={() => router.back()}
       >
         <Image
           width="24"
@@ -16,7 +20,7 @@ export default function HeaderOfCart({ title }: { title: string }) {
           src="https://img.icons8.com/ios/50/left--v1.png"
           alt="backButton"
         />
-      </Link>
+      </div>
       <h3 className="text-[14px] w-full text-center mx-auto relative left-[10px]">
         {title}
       </h3>
@@ -34,14 +38,14 @@ export default function HeaderOfCart({ title }: { title: string }) {
       </Link>
       <Link
         className="w-[50px] h-full items-center flex justify-center"
-        href={"#"}
+        href={"/"}
       >
         <Image
           width="24"
           height="22"
           className="mx-auto"
           src={Home}
-          alt="backButton"
+          alt="home"
         />
       </Link>
     </header>
