061ecab99d0177b8c5600ea6e30138093950bfb5
ddolboghi <rhdfyd128@gmail.com>
2024-04-04 09:09:02 +0900

    refactor: 회원가입 폼 input value 핸들러 수정

--- src/components/form/signupForm/SignupForm.tsx
+++ src/components/form/signupForm/SignupForm.tsx
@@ -26,12 +26,20 @@ import { AgreementsType, MktReceiveMethodsType } from "@/types/agreementType"
 import { useRouter } from "next/navigation"
 import Postcode from "@/components/address/PostCode"
 
+type signupFormType = {
+  signinId: string
+  password: string
+  confirmPassword: string
+}
+
 export default function SignupForm() {
-  const [signinId, setSigninId] = useState("")
+  const [payload, setPayload] = useState<signupFormType>({
+    signinId: "",
+    password: "",
+    confirmPassword: "",
+  })
   const [isDuplId, setIsDuplId] = useState(false)
   const [checkId, setCheckId] = useState(false)
-  const [password, setPassword] = useState<string>("")
-  const [confirmPassword, setConfirmPassword] = useState<string>("")
   const [isOpenAddress, setOpenAddress] = useState<boolean>(false)
   const [fullAddress, setFullAddress] = useState<string>("")
   const [detailAddress, setDetailAddress] = useState<string>("")
@@ -53,14 +61,19 @@ export default function SignupForm() {
   })
   const router = useRouter()
 
-  // TODO: state들을 객체 하나로 묶고 하나의 핸들러로 관리하기, 강사님 로그인폼 참고
+  const onChangePayload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPayload({
+      ...payload,
+      [e.target.name]: e.target.value,
+    })
+  }
 
   //아이디 중복 확인
   const checkIdDuplicate = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>,
   ) => {
     e.preventDefault()
-    const result = await idDuplCheck(signinId)
+    const result = await idDuplCheck(payload.signinId)
     setIsDuplId(result ? false : true)
     setCheckId(true)
   }
@@ -95,9 +108,8 @@ export default function SignupForm() {
     methods: AgreementsType,
   ) => {
     const methodTexts: string[] = Object.entries(agrees)
-      .filter(([key, value]) => value) // 동의한 항목만 필터링
+      .filter(([key, value]) => value)
       .map(([key]) => {
-        //methods에서 해당 항목 찾기
         const method = methods.find((item) => item.id === key)
         return method ? method.text : ""
       })
@@ -108,15 +120,6 @@ export default function SignupForm() {
       result = `${methodTexts.join(", ")}`
     }
     return result
-  }
-
-  function getTodayDate() {
-    const today = new Date()
-    const year = today.getFullYear() // 년도
-    const month = today.getMonth() + 1 // 월 (getMonth()는 0부터 시작하므로 +1)
-    const date = today.getDate() // 일
-
-    return `${year}년 ${month}월 ${date}일`
   }
 
   const handleRoute = () => {
@@ -139,8 +142,8 @@ export default function SignupForm() {
                   type="text"
                   name="signinId"
                   placeholder="영어 또는 숫자로 6~20자리"
-                  value={signinId}
-                  onChange={(e) => setSigninId(e.target.value)}
+                  value={payload.signinId}
+                  onChange={onChangePayload}
                 />
                 <button
                   className="w-24 text-xs text-center bg-[#F8F8F8] border border-slate-300 font-[550]"
@@ -150,7 +153,7 @@ export default function SignupForm() {
                 </button>
               </div>
               <input readOnly hidden name="checkId" value={checkId ? 1 : 0} />
-              {!checkId && signinId.length > 0 && (
+              {!checkId && payload.signinId.length > 0 && (
                 <p className="text-[#FF5452]">아이디 중복확인을 해주세요.</p>
               )}
               <input readOnly hidden name="isDuplId" value={isDuplId ? 1 : 0} />
@@ -174,27 +177,27 @@ export default function SignupForm() {
                 type="password"
                 name="password"
                 placeholder="영문, 숫자 조합 8~20자리"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                value={payload.password}
+                onChange={onChangePayload}
               />
               <input
                 className="grow py-2.5 pl-3 text-xs whitespace-nowrap bg-white border border-solid border-[#D9D9D9]"
                 placeholder="비밀번호 재확인"
                 type="password"
                 name="confirmPassword"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
+                value={payload.confirmPassword}
+                onChange={onChangePayload}
               />
-              {password.length > 0 &&
-                confirmPassword.length > 0 &&
-                password !== confirmPassword && (
+              {payload.password.length > 0 &&
+                payload.confirmPassword.length > 0 &&
+                payload.password !== payload.confirmPassword && (
                   <p className="text-[#FF5452]">
                     비밀번호가 일치하지 않습니다.
                   </p>
                 )}
-              {password.length > 0 &&
-                confirmPassword.length > 0 &&
-                password === confirmPassword && (
+              {payload.password.length > 0 &&
+                payload.confirmPassword.length > 0 &&
+                payload.password === payload.confirmPassword && (
                   <p className="text-[#4fdd43]">비밀번호가 일치합니다.</p>
                 )}
             </dd>
@@ -275,13 +278,13 @@ export default function SignupForm() {
             </dd>
           </dl>
         </section>
-        {/* TODO: 이메일 본인인증하면 이메일 넘겨주기 */}
         <section className="py-4">
           <dl className="flex flex-row h-10 items-center">
             <dt className="w-20">
               <span className="text-[#FF5452]">*</span>이메일주소
             </dt>
             <dd className="grow flex flex-col gap-1">
+              {/* TODO: 이메일 본인인증하면 이메일 넘겨주기 */}
               <input
                 className="grow py-2.5 pl-3 text-xs whitespace-nowrap bg-white border border-solid border-[#D9D9D9]"
                 placeholder="이메일주소"
@@ -293,7 +296,6 @@ export default function SignupForm() {
         </section>
       </section>
       <section className="px-5 text-xs">
-        {/* TODO: 폼 제출시 마케팅 정보 수신 동의한 항목 모달로 보여주기 */}
         <h3 className="py-3.5 bg-[#F8F8F8]">마케팅 정보 수신 동의</h3>
         <div className="py-3.5">
           <h5 className="font-black mb-3">신세계포인트</h5>
@@ -337,7 +339,8 @@ export default function SignupForm() {
                   <br />
                   [마케팅 정보 수신 동의 변경일]
                   <br />
-                  {getTodayDate()}
+                  `${new Date().getFullYear()}년 ${new Date().getMonth() + 1}월
+                  ${new Date().getDate()}일`
                   <br />
                   <br />
                   [마케팅 정보 수신 동의 안내]
