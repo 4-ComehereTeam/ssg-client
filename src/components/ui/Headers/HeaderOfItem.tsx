289d8c97615497f4393ac2c230f410410b6dcebf
ddolboghi <rhdfyd128@gmail.com>
2024-04-04 23:03:46 +0900

    fix: 상품 상세페이지 헤더 수정

--- src/components/ui/Headers/HeaderOfItem.tsx
+++ src/components/ui/Headers/HeaderOfItem.tsx
@@ -2,7 +2,7 @@
 
 import Image from "next/image"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 export default function HeaderOfItem({
   children,
@@ -11,6 +11,59 @@ export default function HeaderOfItem({
 }) {
   const router = useRouter()
   const [showHeader, setShowHeader] = useState(false)
+  const [activeSection, setActiveSection] = useState("")
+
+  useEffect(() => {
+    const header = document.getElementById("header")
+    const detailSection = document.getElementById("descriptionSection")
+    const reviewSection = document.getElementById("reviewSection")
+
+    const callback = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id)
+        }
+      })
+    }
+
+    const observer = new IntersectionObserver(callback, {
+      root: null, // 뷰포트를 기준으로
+      threshold: 1.0, // 완전히 보이는 경우에만 콜백 실행
+    })
+
+    if (detailSection) observer.observe(detailSection)
+    if (reviewSection) observer.observe(reviewSection)
+
+    const handleScroll = () => {
+      const { top: headerTop, bottom: headerBottom } =
+        header.getBoundingClientRect()
+      const { top: detailTop, bottom: detailBottom } =
+        detailSection.getBoundingClientRect()
+      const { top: reviewTop, bottom: reviewBottom } =
+        reviewSection.getBoundingClientRect()
+
+      if (
+        (detailTop <= headerBottom && detailTop >= headerTop) ||
+        (detailBottom <= headerBottom && detailBottom >= headerTop)
+      ) {
+        setActiveSection("detail")
+      } else if (
+        (reviewTop <= headerBottom && reviewTop >= headerTop) ||
+        (reviewBottom <= headerBottom && reviewBottom >= headerTop)
+      ) {
+        setActiveSection("review")
+      } else {
+        setActiveSection("")
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      observer.disconnect()
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
 
   useEffect(() => {
     const handleScroll = () => {
@@ -42,6 +95,7 @@ export default function HeaderOfItem({
 
   return (
     <div
+      id="header"
       className={`fixed top-0 left-0 right-0 transition-opacity duration-300 ${
         showHeader ? "opacity-100" : "opacity-0"
       } bg-white z-[100] flex justify-between items-center pr-[49px] w-full h-[42px] text-sm text-center text-black whitespace-nowrap border-b border-solid border-stone-300`}
@@ -60,11 +114,22 @@ export default function HeaderOfItem({
         <ul className="flex flex-row justify-center gap-8 font-bold">
           <li>
             <div onClick={handleScrollMoveToDesc}>
-              <span>상세</span>
+              <span
+                style={{
+                  color: activeSection === "desc" ? "#FF5452" : "black",
+                }}
+              >
+                상세
+              </span>
             </div>
           </li>
           <li>
-            <div onClick={handleScrollMoveToReview}>
+            <div
+              onClick={handleScrollMoveToReview}
+              style={{
+                color: activeSection === "review" ? "#FF5452" : "black",
+              }}
+            >
               <p className="mb-[-9px]">리뷰</p>
               {children}
             </div>
