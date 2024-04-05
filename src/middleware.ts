f8c7cb0f57a7ae94a8cabc9596d2cbe9ac9e57c9
ddolboghi <rhdfyd128@gmail.com>
2024-04-06 01:53:38 +0900

    feat: 로그인 여부에 따른 페이지 접근 제어

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,6 +4,7 @@ import {
   protectedRoutes,
   apiAuthPrefix,
   DEFAULT_SIGNIN_REDIRECT,
+  authRequeiredRoutes,
 } from "./routes"
 import { getToken } from "next-auth/jwt"
 
@@ -13,12 +14,16 @@ export async function middleware(req: NextRequest, event: NextFetchEvent) {
   const session = await getToken({ req, secret })
   const { nextUrl } = req
 
+  const isRequiredAuth = authRequeiredRoutes.includes(nextUrl.pathname)
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
   const isProtectedRoute = protectedRoutes.includes(nextUrl.pathname)
 
-  //"/api/auth/..."로 접속 후 로그인 페이지로 리다이렉트되므로 이대로 진행
   if (isApiAuthRoute) {
     return null
+  }
+
+  if (!session && isRequiredAuth) {
+    return NextResponse.redirect(new URL("/member/signin", nextUrl))
   }
 
   if (isProtectedRoute) {
