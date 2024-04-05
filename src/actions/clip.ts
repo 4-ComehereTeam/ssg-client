5cc77c07b1b31f223ddffb21603b61fe6a2edc8d
ddolboghi <rhdfyd128@gmail.com>
2024-04-05 17:03:10 +0900

    fix: 관심 상품 개수 fetch함수 삭제 및 좋아요 페이지 하위 컴포넌트 수정

--- src/actions/clip.ts
+++ src/actions/clip.ts
@@ -9,60 +9,83 @@ async function getSession() {
   return session
 }
 
-//좋아요 등록
-export async function postClip(itemId: number) {
+//개별 상품 좋아요 조회
+export async function getClip(itemId: number | string) {
   revalidateTag("getClipItemIds")
   const session = await getSession()
   try {
-    const res = await fetch(
-      `${process.env.API_BASE_URL}/clip/item?itemId=${itemId}`,
-      {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: session?.user.accessToken,
-        },
+    const res = await fetch(`${process.env.API_BASE_URL}/clip/item/${itemId}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: session ? session.user.accessToken : null,
       },
-    )
+    })
+
     if (res.ok) {
       const data = await res.json()
-      console.log("post clip success:", data)
+      console.log("getClip success:", data.httpStatus)
+      return data.result.isCliped
     }
+    console.log("getClip fail", res.status)
+    return false
+  } catch (error) {
+    console.log("getClip fail:", error)
+    return false
+  }
+}
+
+//좋아요 등록
+export async function postClip(itemId: number | string) {
+  revalidateTag("getClipItemIds")
+  const session = await getSession()
+  console.log(session?.user.accessToken)
+  try {
+    const res = await fetch(`${process.env.API_BASE_URL}/clip/item/${itemId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: session ? session.user.accessToken : null,
+      },
+    })
+    if (res.ok) {
+      const data = await res.json()
+      console.log("postClip success:", data.httpStatus)
+    }
+    console.log("postClip status", res.status)
     return null
   } catch (error) {
-    console.log("post clip fail:", error)
+    console.log("postClip fail:", error)
   }
 }
 
 //좋아요 취소
-export async function deleteClip(itemId: number) {
+export async function deleteClip(itemId: number | string) {
   revalidateTag("getClipItemIds")
   const session = await getSession()
   try {
-    const res = await fetch(
-      `${process.env.API_BASE_URL}/clip/item?itemId=${itemId}`,
-      {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: session?.user.accessToken,
-        },
+    const res = await fetch(`${process.env.API_BASE_URL}/clip/item/${itemId}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: session ? session.user.accessToken : null,
       },
-    )
+    })
 
     if (res.ok) {
       const data = await res.json()
-      console.log("delete clip success:", data)
+      console.log("deleteClip success:", data)
       return data
     }
+    console.log("deleteClip status", res.status)
     return null
   } catch (error) {
-    console.log("delete clip fail:", error)
+    console.log("deleteClip fail:", error)
   }
 }
 
 //좋아요 여러 개 취소
-export async function deleteManyClips(itemIds: number[]) {
+export async function deleteManyClips(itemIds: number[] | string[]) {
   revalidateTag("getClipItemIds")
   const session = await getSession()
   try {
@@ -70,7 +93,7 @@ export async function deleteManyClips(itemIds: number[]) {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
-        Authorization: session?.user.accessToken,
+        Authorization: session ? session.user.accessToken : null,
       },
       body: JSON.stringify({
         itemIds: itemIds,
