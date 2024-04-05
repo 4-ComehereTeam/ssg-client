29a0de9b505b3fb90e8e000ecdc5808e123f55f9
ddolboghi <rhdfyd128@gmail.com>
2024-04-05 17:04:59 +0900

    fix: 좋아요 페이지 데이터 페칭에 따른 컴포넌트 수정

--- src/components/clip/ClipNavbar.tsx
+++ src/components/clip/ClipNavbar.tsx
@@ -6,21 +6,21 @@ type ClipNavbarProps = {
   clipNums: ClipNumsType
 }
 
-export default function ClipNavbar({ clipNums }: ClipNavbarProps) {
+export default function ClipNavbar({ clipNums }: { clipNums: number }) {
   //브랜드, 카테고리 좋아요는 페이지 따로 만들기
   return (
     <nav className="text-sm bg-[#F5F5F5]">
       <ul className="px-4 py-5 flex flex-row gap-3">
         <li>
           <button>
-            <span>상품 ({clipNums.item})</span>
+            <span>상품 ({clipNums})</span>
           </button>
         </li>
         <li>
-          <span>브랜드&스토어 ({clipNums.brand})</span>
+          <span>브랜드&스토어 (0)</span>
         </li>
         <li>
-          <span>카테고리 ({clipNums.category})</span>
+          <span>카테고리 (0)</span>
         </li>
       </ul>
     </nav>
