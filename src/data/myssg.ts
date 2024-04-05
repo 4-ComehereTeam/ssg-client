8e1c134604b4bef2922349b53ff7948517613cd2
ddolboghi <rhdfyd128@gmail.com>
2024-04-05 17:06:52 +0900

    fix: 마이페이지 자주 찾는 메뉴 데이터 수정 좋아요 페이지 라우트 연결

--- src/data/myssg.ts
+++ src/data/myssg.ts
@@ -1,78 +1,93 @@
-export const freqMenuIcon = [
+export const freqMenu = [
   {
     id: 1,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_like.svg",
     alt: "좋아요",
+    href: "/myssg/clip",
   },
   {
     id: 2,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_freq.svg",
     alt: "자주구매",
+    href: "#",
   },
   {
     id: 3,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_event.svg",
     alt: "이벤트현황",
+    href: "#",
   },
   {
     id: 4,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_qna.svg",
     alt: "상품Q&A",
+    href: "#",
   },
   {
     id: 5,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_alert.svg",
     alt: "입고알림",
+    href: "#",
   },
   {
     id: 6,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_gift.svg",
     alt: "선물함",
+    href: "#",
   },
   {
     id: 7,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_giftcard.svg",
     alt: "상품권전환",
+    href: "#",
   },
   {
     id: 8,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_albi.svg",
     alt: "알비백관리",
+    href: "#",
   },
   {
     id: 9,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_delivery.svg",
     alt: "배송지관리",
+    href: "#",
   },
   {
     id: 10,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_regular_delivery.svg",
     alt: "정기배송관리",
+    href: "#",
   },
   {
     id: 11,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_notification.svg",
     alt: "알림함",
+    href: "#",
   },
   {
     id: 12,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_chat.svg",
     alt: "고객센터톡",
+    href: "#",
   },
   {
     id: 13,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_universe.svg",
     alt: "유니버스클럽",
+    href: "#",
   },
   {
     id: 14,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_modify.svg",
     alt: "회원정보변경",
+    href: "#",
   },
   {
     id: 15,
     src: "https://sui.ssgcdn.com/ui/m_ssg/img/myssg_main/img_manage.svg",
     alt: "내정보관리",
+    href: "#",
   },
 ]
 
@@ -107,7 +122,7 @@ export const manageList = [
     title: "나의 활동관리",
 
     items: [
-      { name: "좋아요", src: "/" },
+      { name: "좋아요", src: "/myssg/clip" },
       { name: "상품리뷰", src: "" },
       { name: "새벽배송 알비백 관리", src: "" },
       { name: "이벤트 참여현황", src: "" },
