1eaaf538745c22a6a7e77f83dcb5993f73f51260
ddolboghi <rhdfyd128@gmail.com>
2024-04-05 17:03:10 +0900

    fix: 관심 상품 개수 fetch함수 삭제 및 좋아요 페이지 하위 컴포넌트 수정

--- src/app/(footer)/myssg/clip/page.tsx
+++ src/app/(footer)/myssg/clip/page.tsx
@@ -2,81 +2,50 @@ import { options } from "@/app/api/auth/[...nextauth]/options"
 import ClipHeader from "@/components/clip/ClipHeader"
 import ClipMain from "@/components/clip/ClipMain"
 import ClipNavbar from "@/components/clip/ClipNavbar"
+import ItemCard from "@/components/item/ItemCard"
 import { getServerSession } from "next-auth"
 import React from "react"
 
 async function getClipItemIds(accessToken: string) {
   try {
-    const res = await fetch(
-      "https://c47b4d94-6da3-46ae-9205-95a73f06e76b.mock.pstmn.io/clip-cancle",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: accessToken,
-        },
-        next: { tags: ["getClipItemIds"] },
+    const res = await fetch(`${process.env.API_BASE_URL}/clip/items`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: accessToken,
       },
-    )
-    if (res.ok) {
-      const data = await res.json()
-      console.log(data)
-      // return data.itemIds
-      return [1, 2, 3, 4] //예시
-    } else {
-      return []
-    }
-  } catch (error) {
-    console.log(error)
-    return []
-  }
-}
+      next: { tags: ["getClipItemIds"] },
+    })
 
-async function getClipNums(accessToken: string) {
-  try {
-    const res = await fetch(
-      "https://c47b4d94-6da3-46ae-9205-95a73f06e76b.mock.pstmn.io/clip-cancle",
-      {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: accessToken,
-        },
-        next: { tags: ["clip", "clipCancle", "manyClipCancle"] },
-      },
-    )
     if (res.ok) {
       const data = await res.json()
-      console.log(data)
-      return data.clipNums
-      /**
-       // return 예시
-       {
-          item: 4,
-          brand: 0,
-          category: 0,
-        }
-       * */
-    } else {
-      return {}
+      console.log("getClipItemIds success", data.httpStatus)
+      // return data.itemIds
+      return data.result.itemIds
     }
+    console.log("getClipItemIds fail", res.status)
+    return []
   } catch (error) {
-    console.log(error)
-    return {}
+    console.log("getClipItemIds fail(error)", error)
+    return []
   }
 }
 
 export default async function ClipPage() {
   const session = await getServerSession(options)
-  const itemIds = await getClipItemIds(session?.user.accessToken)
-  const clipNums = await getClipNums(session?.user.accessToken)
+  const accessToken = session ? session.user.accessToken : null
+  const itemIds = await getClipItemIds(accessToken)
 
   //Header 추가하기
   return (
     <>
       <ClipHeader />
-      <ClipNavbar clipNums={clipNums} />
-      <ClipMain itemIds={itemIds} />
+      <ClipNavbar clipNums={itemIds.length} />
+      <ClipMain itemIds={itemIds}>
+        {itemIds.map((itemId: number) => (
+          <ItemCard key={itemId} itemId={itemId} />
+        ))}
+      </ClipMain>
     </>
   )
 }
