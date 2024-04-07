9e6d6033fa2bfa0446a153743f7321cc890cb66a
ddolboghi <rhdfyd128@gmail.com>
2024-04-07 12:27:46 +0900

    fix: 인증된 사용자 페이지 접근 제어 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,38 +1,22 @@
 import type { NextRequest, NextFetchEvent } from "next/server"
 import { NextResponse } from "next/server"
-import {
-  protectedRoutes,
-  apiAuthPrefix,
-  DEFAULT_SIGNIN_REDIRECT,
-  authRequeiredRoutes,
-} from "./routes"
-import { getToken } from "next-auth/jwt"
-
-const secret = process.env.NEXTAUTH_SECRET
+import { apiAuthPrefix, authRequeiredRoutes } from "./routes"
 
 export async function middleware(req: NextRequest, event: NextFetchEvent) {
-  const session = await getToken({ req, secret })
+  const accessToken = req.cookies.get("next-auth.session-token")
   const { nextUrl } = req
 
-  const isRequiredAuth = authRequeiredRoutes.includes(nextUrl.pathname)
+  const isRequiredAuth = authRequeiredRoutes.some((authRequeiredRoute) =>
+    nextUrl.pathname.startsWith(authRequeiredRoute),
+  )
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-  const isProtectedRoute = protectedRoutes.includes(nextUrl.pathname)
 
   if (isApiAuthRoute) {
     return null
   }
 
-  if (!session && isRequiredAuth) {
+  if (!accessToken && isRequiredAuth) {
     return NextResponse.redirect(new URL("/member/signin", nextUrl))
-  }
-
-  if (isProtectedRoute) {
-    if (session) {
-      return NextResponse.redirect(
-        new URL(DEFAULT_SIGNIN_REDIRECT, nextUrl), //TODO: 이전 페이지로 리다이렉트하기
-      )
-    }
-    return null
   }
 }
 
