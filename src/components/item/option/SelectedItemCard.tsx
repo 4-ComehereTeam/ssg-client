3922d02ff78b2d12b704f6c41b405a4cb0c8a557
ddolboghi <rhdfyd128@gmail.com>
2024-04-08 20:21:14 +0900

    feat: 선택된 옵션들 표시 및 개수 수정 기능 구현
    fix: 옵션 카드 이름 수정

--- src/components/item/option/SelectedItemCard.tsx
+++ src/components/item/option/SelectedItemCard.tsx
@@ -1,72 +1,66 @@
 "use client"
 
 import { ItemBasicInfo } from "@/actions/item"
-import { ItemOption } from "@/actions/itemOption"
-import Image from "next/image"
-import { useState } from "react"
+import { ItemOptions } from "../ItemBottomBar"
 
 type SelectedItemCardProps = {
-  itemOptions: ItemOption[]
-  itemBasicInfo: ItemBasicInfo | null
-  handleItemOptions: (index: number) => void
+  itemBasicInfo: ItemBasicInfo
+  itemNoneOptions: ItemOptions
+  handleItemOptionCount: (itemOptionId: number, count: number) => void
 }
 
-export default function SelectedItemCard({
-  itemOptions,
+export default function SeletedItemCard({
   itemBasicInfo,
-  handleItemOptions,
+  itemNoneOptions,
+  handleItemOptionCount,
 }: SelectedItemCardProps) {
-  const [itemCount, setItemCount] = useState(1)
+  const itemNoneOption = itemNoneOptions[0]
   const discountRate = itemBasicInfo ? itemBasicInfo.discountRate : 0
   const price = itemBasicInfo ? itemBasicInfo.price : 0
   const discountPrice =
     discountRate !== 0 ? price * ((100 - discountRate) / 100) : price
-  const finalPrice = new Intl.NumberFormat().format(
-    Math.round(discountPrice * itemCount),
+  const totalPrice = new Intl.NumberFormat().format(
+    discountPrice * itemNoneOption.count,
   )
-
   return (
-    <div className="mt-4">
-      {itemOptions.map((itemOption, index) => (
-        <div key={itemOption.itemOptionId} className="px-4">
-          <div className="py-4 flex flex-col gap-2 border border-black rounded-md bg-[#f8f8f8f8]">
-            <div className="px-4 flex flex-row justify-between ">
-              <p>
-                <span className="text-xs">색상: {itemOption.color}</span>
-                {itemOption.size && <span> / 사이즈: {itemOption.size}</span>}
-                {itemOption.etc && <span> / 기타: {itemOption.etc}</span>}
-              </p>
-              <div onClick={() => handleItemOptions(index)}>
-                <Image
-                  width={16}
-                  height={16}
-                  src="https://img.icons8.com/sf-ultralight/25/delete-sign.png"
-                  alt="삭제"
-                />
-              </div>
+    <div className="mt-4 mb-24 flex flex-col gap-2">
+      <div className="px-4">
+        <div className="py-4 flex flex-col gap-3 rounded-md bg-[#f8f8f8f8]">
+          <div className="px-4 flex flex-row justify-between ">
+            <p className="text-xs">{itemBasicInfo.itemName}</p>
+          </div>
+          <div className="px-4 flex flex-row justify-between items-center">
+            <div className="basis-1/3 bg-white flex flex-row justify-between items-center">
+              <button
+                className="w-[34px] h-[33px] text-[20px]"
+                onClick={() =>
+                  handleItemOptionCount(
+                    itemNoneOption.itemOptionId,
+                    itemNoneOption.count - 1,
+                  )
+                }
+                disabled={itemNoneOption.count < 2}
+              >
+                -
+              </button>
+              <span>{itemNoneOption.count}</span>
+              <button
+                className="w-[34px] h-[33px] text-[20px]"
+                onClick={() =>
+                  handleItemOptionCount(
+                    itemNoneOption.itemOptionId,
+                    itemNoneOption.count + 1,
+                  )
+                }
+                disabled={itemNoneOption.stock <= itemNoneOption.count}
+              >
+                +
+              </button>
             </div>
-            <div className="px-4 flex flex-row justify-between items-center">
-              <div className="basis-1/3 bg-white flex flex-row justify-between items-center">
-                <button
-                  className="w-[34px] h-[33px] text-[20px]"
-                  onClick={() => setItemCount(itemCount - 1)}
-                  disabled={itemCount < 2}
-                >
-                  -
-                </button>
-                <span>{itemCount}</span>
-                <button
-                  className="w-[34px] h-[33px] text-[20px]"
-                  onClick={() => setItemCount(itemCount + 1)}
-                >
-                  +
-                </button>
-              </div>
-              <span className="font-bold">{finalPrice}원</span>
-            </div>
+            <span className="font-bold">{totalPrice}원</span>
           </div>
         </div>
-      ))}
+      </div>
     </div>
   )
 }
