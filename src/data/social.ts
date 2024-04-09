34dc2e0054254564c9af1005e48fd4c695fd1a13
ddolboghi <rhdfyd128@gmail.com>
2024-04-09 21:50:34 +0900

    fix: 네이버, 카카오 주석 처리

--- src/data/social.ts
+++ src/data/social.ts
@@ -4,18 +4,18 @@ import googleLogo from "@/asset/images/google.png"
 import eamilLogo from "@/asset/images/simple-signup-email.png"
 
 export const socialSignin = [
-  {
-    src: naverLogo,
-    alt: "네이버 로그인",
-    text: "네이버",
-    provider: "naver",
-  },
-  {
-    src: kakaoLogo,
-    alt: "카카오 로그인",
-    text: "카카오",
-    provider: "kakao",
-  },
+  // {
+  //   src: naverLogo,
+  //   alt: "네이버 로그인",
+  //   text: "네이버",
+  //   provider: "naver",
+  // },
+  // {
+  //   src: kakaoLogo,
+  //   alt: "카카오 로그인",
+  //   text: "카카오",
+  //   provider: "kakao",
+  // },
   {
     src: googleLogo,
     alt: "구글 로그인",
@@ -25,24 +25,24 @@ export const socialSignin = [
 ]
 
 export const socialSignup = [
-  {
-    src: eamilLogo,
-    alt: "이메일 회원가입",
-    text: "이메일",
-    provider: "",
-  },
-  {
-    src: naverLogo,
-    alt: "네이버 회원가입",
-    text: "네이버",
-    provider: "naver",
-  },
-  {
-    src: kakaoLogo,
-    alt: "카카오 회원가입",
-    text: "카카오",
-    provider: "kakao",
-  },
+  // {
+  //   src: eamilLogo,
+  //   alt: "이메일 회원가입",
+  //   text: "이메일",
+  //   provider: "",
+  // },
+  // {
+  //   src: naverLogo,
+  //   alt: "네이버 회원가입",
+  //   text: "네이버",
+  //   provider: "naver",
+  // },
+  // {
+  //   src: kakaoLogo,
+  //   alt: "카카오 회원가입",
+  //   text: "카카오",
+  //   provider: "kakao",
+  // },
   {
     src: googleLogo,
     alt: "구글 회원가입",
