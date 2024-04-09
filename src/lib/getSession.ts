3e6fae7debb2aded7d4b45fdfe79e25491c10130
ddolboghi <rhdfyd128@gmail.com>
2024-04-10 00:25:55 +0900

    fix: getSession 주석 추가

--- src/lib/getSession.ts
+++ src/lib/getSession.ts
@@ -5,7 +5,7 @@ import { getServerSession } from "next-auth"
 
 /**
  * 서버컴포넌트 또는 서버 사이드에서만 호출
- * @return {Session | null} Session - 사용자 정보가 들어있는 Object로, 변수에 담아 프로퍼티에 접근할 수 있음
+ * @return {Session | null} Session - 사용자 정보가 들어있는 Object로, 변수에 담아 프로퍼티에 접근할 수 있음.
  * 예시: { user: { name: 'Jobs', email: 'Jobs@ex.com', signinId: '....', id: '...', accessToken: '...', ... }}
  */
 export async function getSession() {
