8c511bbf10df0c39a8f9f46ae55569330e2426ee
ddolboghi <rhdfyd128@gmail.com>
2024-04-11 03:25:24 +0900

    fix: 카테고리 페이지 수정

--- src/app/(nofooter)/category/page.tsx
+++ src/app/(nofooter)/category/page.tsx
@@ -1,5 +1,11 @@
+import { getBigCategories } from "@/actions/category/category"
 import CategoryTable from "@/components/category/CategoryTable"
+import { redirect } from "next/navigation"
 
-export default function CategoryPage() {
-  return <CategoryTable />
+export default async function CategoryPage() {
+  const result = await getBigCategories()
+  if (!result) {
+    redirect("/not-found")
+  }
+  return <CategoryTable bigCategoriesData={result} />
 }
