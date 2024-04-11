b4b28d7c0b819ab9986893e87454b551bb04dd84
ddolboghi <rhdfyd128@gmail.com>
2024-04-11 11:26:25 +0900

    fix: 카테고리 페이지 구성 컴포넌트 수정

--- src/components/category/CategoryTable.tsx
+++ src/components/category/CategoryTable.tsx
@@ -1,24 +1,47 @@
+"use client"
+
 import { BigCategoriesData } from "@/actions/category/category"
 import BigCategory from "./BigCategory"
+import { useState } from "react"
 
 export default function CategoryTable({
   bigCategoriesData,
 }: {
   bigCategoriesData: BigCategoriesData
 }) {
+  const [openMiddles, setOpenMiddles] = useState(
+    Array.from({ length: bigCategoriesData.count }, (_, index) => ({
+      id: index + 1,
+      isOpen: false,
+    })),
+  )
+  const handleMiddleCategories = async (bigCategoryId: number) => {
+    setOpenMiddles((prevOpenMiddles) =>
+      prevOpenMiddles.map((ctg) =>
+        ctg.id === bigCategoryId
+          ? { ...ctg, isOpen: true }
+          : { ...ctg, isOpen: false },
+      ),
+    )
+  }
   return (
     <div className="h-screen">
-      <div className="grid grid-cols-5 pt-[15px] pr-[10px] pb-[25px] pl-[10px]">
+      <ul className="grid grid-cols-5 auto-rows-auto pt-[15px] pr-[10px] pb-[25px] pl-[10px]">
         {bigCategoriesData.bigCategories.map((big) => (
-          <div key={big.id} className="py-2 min-h-[80px]">
+          <li
+            key={big.id}
+            className="py-2 min-h-[80px]"
+            onClick={() => handleMiddleCategories(big.id)}
+          >
             <BigCategory
               bigCategoryId={big.id}
               bigCategoryName={big.name}
               bigCategoriesCount={bigCategoriesData.count}
+              isOpen={openMiddles.find((ctg) => ctg.id === big.id)?.isOpen}
             />
-          </div>
+          </li>
         ))}
-      </div>
+      </ul>
     </div>
   )
 }
--- src/components/category/MiddleCategory.tsx
+++ src/components/category/MiddleCategory.tsx
@@ -1,38 +1,63 @@
-import { MiddleCategories } from "@/actions/category/category"
+"use client"
+
+import {
+  getMiddleCategories,
+  MiddleCategories,
+} from "@/actions/category/category"
 import Link from "next/link"
+import { useEffect, useState } from "react"
 
 export default function MiddleCategory({
   bigCategoryId,
-  middleCategories,
 }: {
   bigCategoryId: number
-  middleCategories: MiddleCategories
 }) {
-  return (
-    <div className="static left-0 overflow-hidden h-auto w-full">
-      {middleCategories && (
-        <ul className="text-xs flex w-full flex-wrap px-3 py-3 bg-gray-100 my-[5px] box-border">
-          <li className="w-1/2 min-h-[38px] flex items-center pl-3 pr-[13px]">
-            <p>
-              <Link href={"/category/all"} passHref>
-                상품 전체보기
-              </Link>
-            </p>
-          </li>
-          {middleCategories.map((mid) => (
-            <li
-              key={mid.id}
-              className="w-1/2 min-h-[38px] flex items-center pl-3 pr-[13px]"
-            >
+  const [middleCategories, setMiddleCatogories] = useState<MiddleCategories>([])
+  const [isVisible, setIsVisible] = useState(false)
+
+  const handleMiddleCategories = async () => {
+    const result = await getMiddleCategories(bigCategoryId)
+    if (result) {
+      setMiddleCatogories(result.middleCategories)
+    }
+  }
+
+  useEffect(() => {
+    handleMiddleCategories()
+    setIsVisible(true)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
+  const visibilityClass = isVisible ? "opacity-100" : "opacity-0"
+  if (middleCategories) {
+    return (
+      <div
+        className={`relative left-0 z-50 h-auto w-[100vw] transition-opacity duration-500 ${visibilityClass}`}
+      >
+        {middleCategories && (
+          <ul className="text-xs flex w-full flex-wrap px-3 py-3 bg-gray-100 my-[5px] box-border">
+            <li className="w-1/2 min-h-[38px] flex items-center pl-3 pr-[13px]">
               <p>
-                <Link href={`/category/${bigCategoryId}/${mid.id}`} passHref>
-                  {mid.name}
+                <Link href={"/category/all"} passHref>
+                  상품 전체보기
                 </Link>
               </p>
             </li>
-          ))}
-        </ul>
-      )}
-    </div>
-  )
+            {middleCategories.map((mid) => (
+              <li
+                key={mid.id}
+                className="w-1/2 min-h-[38px] flex items-center pl-3 pr-[13px]"
+              >
+                <p>
+                  <Link href={`/category/${bigCategoryId}/${mid.id}`} passHref>
+                    {mid.name}
+                  </Link>
+                </p>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    )
+  }
 }
