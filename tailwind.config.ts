546de235e8850fc2cbb140b31c35afd86b2a48b5
ddolboghi <rhdfyd128@gmail.com>
2024-04-12 01:10:58 +0900

    chore: tailwind fade-in 애니메이션 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -87,7 +87,7 @@ const config = {
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
-        "fade-in": "fade-in 0.5s ease-out",
+        "fade-in": "fade-in 0.3s ease-out",
         skeleton: "skeleton 1.5s ease-in-out infinite",
       },
     },
