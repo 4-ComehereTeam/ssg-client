4c6e9fbf539b16b766ba0b6cd60fed50363c0882
ddolboghi <rhdfyd128@gmail.com>
2024-04-11 14:25:56 +0900

    fix: 카테고리 페이지 중분류 위치 수정

---
+++ src/components/category/CategorySection.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import {
+  BigCategoriesData,
+  getMiddleCategories,
+  MiddleCategories,
+} from "@/actions/category/category"
+import CategoryTable from "./CategoryTable"
+import { useState } from "react"
+
+export default function CategorySection({
+  bigCategoriesData,
+}: {
+  bigCategoriesData: BigCategoriesData
+}) {
+  const row = Math.ceil(bigCategoriesData.count / 5)
+  const [openMiddles, setOpenMiddles] = useState(
+    Array.from({ length: bigCategoriesData.count }, (_, index) => ({
+      id: index + 1,
+      isOpen: false,
+    })),
+  )
+  const [middleCategories, setMiddleCategories] = useState<MiddleCategories>([])
+
+  const handleMiddleCategories = async (bigCategoryId: number) => {
+    setOpenMiddles((prevOpenMiddles) =>
+      prevOpenMiddles.map((ctg) =>
+        ctg.id === bigCategoryId
+          ? { ...ctg, isOpen: true }
+          : { ...ctg, isOpen: false },
+      ),
+    )
+    const result = await getMiddleCategories(bigCategoryId)
+    if (result) {
+      setMiddleCategories(result.middleCategories)
+    }
+  }
+
+  const openBigId = openMiddles.find((mid) => mid.isOpen)?.id
+  const isOpenMid = openMiddles.find((mid) => mid.isOpen)?.isOpen
+
+  console.log(openMiddles)
+  return (
+    <>
+      {Array.from({ length: row }, (_, index) => {
+        const start = index * 5
+        const end = start + 5
+        return (
+          <CategoryTable
+            key={index}
+            bigCategoriesData={bigCategoriesData}
+            start={start}
+            end={end}
+            handleMiddleCategories={handleMiddleCategories}
+            isOpenMid={isOpenMid}
+            openBigId={openBigId}
+            middleCategories={middleCategories}
+          />
+        )
+      })}
+    </>
+  )
+}
--- src/components/category/CategoryTable.tsx
+++ src/components/category/CategoryTable.tsx
@@ -1,34 +1,36 @@
 "use client"
 
-import { BigCategoriesData } from "@/actions/category/category"
+import {
+  BigCategoriesData,
+  MiddleCategories,
+} from "@/actions/category/category"
 import BigCategory from "./BigCategory"
-import { useState } from "react"
+import MiddleCategory from "./MiddleCategory"
 
 export default function CategoryTable({
   bigCategoriesData,
+  start,
+  end,
+  handleMiddleCategories,
+  isOpenMid,
+  openBigId,
+  middleCategories,
 }: {
   bigCategoriesData: BigCategoriesData
+  start: number
+  end: number
+  handleMiddleCategories: (bigCategoryId: number) => Promise<void>
+  isOpenMid: boolean | undefined
+  openBigId: number | undefined
+  middleCategories: MiddleCategories
 }) {
-  const [openMiddles, setOpenMiddles] = useState(
-    Array.from({ length: bigCategoriesData.count }, (_, index) => ({
-      id: index + 1,
-      isOpen: false,
-    })),
-  )
-  const handleMiddleCategories = async (bigCategoryId: number) => {
-    setOpenMiddles((prevOpenMiddles) =>
-      prevOpenMiddles.map((ctg) =>
-        ctg.id === bigCategoryId
-          ? { ...ctg, isOpen: true }
-          : { ...ctg, isOpen: false },
-      ),
-    )
-  }
+  const isCurrentRow = (bigCategoryId: number) =>
+    bigCategoryId >= start + 1 && bigCategoryId <= end
   return (
-    <div className="h-screen">
-      <ul className="grid grid-cols-5 auto-rows-auto pt-[15px] pr-[10px] pb-[25px] pl-[10px]">
-        {bigCategoriesData.bigCategories.map((big) => (
-          <li
+    <div>
+      <div className="grid grid-cols-5 auto-rows-auto px-[10px]">
+        {bigCategoriesData.bigCategories.slice(start, end).map((big) => (
+          <div
             key={big.id}
             className="py-2 min-h-[80px]"
             onClick={() => handleMiddleCategories(big.id)}
@@ -37,11 +39,17 @@ export default function CategoryTable({
               bigCategoryId={big.id}
               bigCategoryName={big.name}
               bigCategoriesCount={bigCategoriesData.count}
-              isOpen={openMiddles.find((ctg) => ctg.id === big.id)?.isOpen}
             />
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
+      {isOpenMid && openBigId && isCurrentRow(openBigId) && (
+        <MiddleCategory
+          bigCategoryId={openBigId}
+          middleCategories={middleCategories}
+          isOpen={isOpenMid}
+        />
+      )}
     </div>
   )
 }
