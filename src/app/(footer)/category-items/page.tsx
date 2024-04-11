2cd3cd008d9e2acd3563595d957c45c34432eb0d
ddolboghi <rhdfyd128@gmail.com>
2024-04-11 21:14:58 +0900

    fix: 카테고리 상품 목록 페이지 수정

---
+++ src/app/(footer)/category-items/page.tsx
@@ -0,0 +1,109 @@
+import SubCategorySlideButton from "@/components/category/SubCategorySlideButton"
+import ProductList from "@/components/category/ProductList"
+import SuperSubCategoryTable from "@/components/category/SuperSubCategoryTable"
+import CategoryProductListToolBar from "@/components/category/CategoryProductListToolBar"
+import {
+  getBigCategoryName,
+  getMidCategoryName,
+  getMiddleCategories,
+  getSmallCategories,
+  getSmallCategoryName,
+} from "@/actions/category/category"
+import { redirect } from "next/navigation"
+
+function validSearchParams(searchParams: {
+  big?: number
+  mid?: number
+  small?: number
+}): boolean {
+  /**
+   * 하위 카테고리 id만으로 접근 불가
+   * big -> 전체보기
+   * big & mid -> 중분류까지
+   * big & mid & small -> 소분류까지
+   */
+  if (searchParams.mid && !searchParams.big) {
+    return false
+  }
+  if (searchParams.small && (!searchParams.mid || !searchParams.big)) {
+    return false
+  }
+  return true
+}
+
+export default async function CategoryProductListPage({
+  searchParams,
+}: {
+  searchParams: { big: number; mid: number; small: number }
+}) {
+  console.log("searchParams:", searchParams)
+  if (!validSearchParams(searchParams)) {
+    redirect("/not-found")
+  }
+  let bigCategoryName = ""
+  let midCategoryName = ""
+  let smallCategoryName = ""
+  if (searchParams.big) {
+    const res = await getBigCategoryName(searchParams.big)
+    if (res) {
+      bigCategoryName = res.categoryName
+    }
+  }
+  if (searchParams.mid) {
+    const res = await getMidCategoryName(searchParams.mid)
+    if (res) {
+      midCategoryName = res.categoryName
+    }
+  }
+  if (searchParams.small) {
+    const res = await getSmallCategoryName(searchParams.small)
+    if (res) {
+      smallCategoryName = res.categoryName
+    }
+  }
+
+  const middleCategoriesData = await getMiddleCategories(searchParams.big)
+  let smallCategoriesData = null
+  if (searchParams.mid) {
+    smallCategoriesData = await getSmallCategories(searchParams.mid)
+  }
+  let subCategories = middleCategoriesData
+    ? middleCategoriesData.middleCategories
+    : null
+  if (searchParams.small) {
+    subCategories = smallCategoriesData
+      ? smallCategoriesData.smallCategories
+      : null
+  }
+  let superSubCategories = smallCategoriesData
+    ? smallCategoriesData.smallCategories
+    : null
+
+  return (
+    <div className="min-h-screen">
+      <div className="contents">
+        <CategoryProductListToolBar
+          bigCategoryName={bigCategoryName}
+          midCategoryName={midCategoryName}
+          smallCategoryName={smallCategoryName}
+        />
+        {subCategories && (
+          <SubCategorySlideButton
+            bigCtgId={searchParams.big}
+            midCtgId={searchParams.mid}
+            existingSmall={searchParams.small ? true : false}
+            subCategories={subCategories}
+          />
+        )}
+        {!searchParams.small && superSubCategories && (
+          <SuperSubCategoryTable
+            bigCtgId={searchParams.big}
+            midCtgId={searchParams.mid}
+            superSubCategories={superSubCategories}
+          />
+        )}
+      </div>
+      <ProductList categoryIds={searchParams} />
+    </div>
+  )
+}
