34323825ad39df82743803a85b9b774f34e34d38
ddolboghi <rhdfyd128@gmail.com>
2024-04-12 17:52:24 +0900

    fix: 페이지 접근 제어 수정

--- src/app/(footer)/member/signup/social/page.tsx
+++ src/app/(footer)/member/signup/social/page.tsx
@@ -3,6 +3,7 @@ import { options } from "@/app/api/auth/[...nextauth]/options"
 import SocialSignupForm from "@/components/form/signupForm/SocialSignupForm"
 import HeaderToBack from "@/components/ui/Headers/HeaderToBack"
 import { getSession } from "@/lib/getSession"
+import { SIGNIN_WITH_CALLBACK } from "@/routes"
 import { getServerSession } from "next-auth"
 import { redirect } from "next/navigation"
 
@@ -10,9 +11,10 @@ export default async function SimpleSignupFormPage() {
   const session = await getServerSession(options)
   if (session) {
     const session = await getSession()
+    console.log(session)
     const isExistId = await idDuplCheck(session?.user.id)
     if (isExistId) {
-      redirect("/not-found")
+      redirect(SIGNIN_WITH_CALLBACK)
     }
   }
   return (
