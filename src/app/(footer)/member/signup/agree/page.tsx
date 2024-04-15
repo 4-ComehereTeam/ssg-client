c2cf2aa600220c0ff4062d7667fcb98d77341e33
ddolboghi <rhdfyd128@gmail.com>
2024-04-15 10:18:29 +0900

    fix: 인증된 사용자는 접근하지 못하도록 수정

--- src/app/(footer)/member/signup/agree/page.tsx
+++ src/app/(footer)/member/signup/agree/page.tsx
@@ -1,8 +1,14 @@
 import SignupAgreeForm from "@/components/form/signupForm/SignupAgreeForm"
 import HeaderToBack from "@/components/ui/Headers/HeaderToBack"
+import { getSession } from "@/lib/getSession"
+import { redirect } from "next/navigation"
 import React from "react"
 
 export default async function SignupAgreePage() {
+  const session = await getSession()
+  if (session?.user.accessToken) {
+    redirect("/not-found")
+  }
   return (
     <>
       <HeaderToBack title="신세계포인트 통합회원 가입" />
