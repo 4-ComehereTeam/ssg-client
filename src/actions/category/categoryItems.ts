f9feb55791a9a3041c7f851c5764873e49d678d3
ddolboghi <rhdfyd128@gmail.com>
2024-04-15 22:55:13 +0900

    fix: API 통신 함수 모음 파일에 서버액션 명시

--- src/actions/category/categoryItems.ts
+++ src/actions/category/categoryItems.ts
@@ -1,3 +1,5 @@
+"use server"
+
 export type CategoryItemsData = {
   itemIds: number[]
   currentPage: number
--- src/actions/itemOption.ts
+++ src/actions/itemOption.ts
@@ -1,3 +1,5 @@
+"use server"
+
 export type OptionExist = {
   itemId: number
   hasColor: boolean
