84da5ae822bd94157811273cea2699a3d881b5bf
ddolboghi <rhdfyd128@gmail.com>
2024-04-15 22:50:18 +0900

    feat: 아이디/비밀번호 찾기 페이지 접근 가능 조건 추가

--- src/app/(footer)/member/findIdPw/page.tsx
+++ src/app/(footer)/member/findIdPw/page.tsx
@@ -1,7 +1,13 @@
 import FindIdPwSection from "@/components/form/findIdPwForm/FindIdPwSection"
 import HeaderToBackNotSticky from "@/components/ui/Headers/HeaderToBackNotSticky"
+import { getSession } from "@/lib/getSession"
+import { redirect } from "next/navigation"
 
 async function Page({ searchParams }: { searchParams: string }) {
+  const session = await getSession()
+  if (session?.user.accessToken) {
+    redirect("/not-found")
+  }
   const isFindId = Object.keys(searchParams)[0] === "id" ? true : false
   return (
     <>
