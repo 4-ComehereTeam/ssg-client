b88826761905a5b855e3d9e626766074d39ee395
ddolboghi <rhdfyd128@gmail.com>
2024-04-15 10:25:39 +0900

    fix: 사용하지 않는 라우트 삭제

--- src/routes.ts
+++ src/routes.ts
@@ -1,31 +1,8 @@
-/**
- * 로그인이 필요한 라우트들
- * @type {string[]} 전체 라우트 또는 공통 라우트
- */
-export const authRequeiredRoutes = ["/myssg/", "/member/resign"]
-
-/**
- * 인증된 사용자는 접근할 수 없는 라우트들
- * @type {string[]}
- */
-export const protectedRoutes = [
-  "/member/findIdPw",
-  "/member/signin",
-  "/member/signup/intro",
-  "/member/signup/agree",
-  "/member/signup/form",
-]
-
-/**
- * 이 값으로 시작하는 라우트는 authentication API에 사용됨
- * @type {string}
- */
-export const apiAuthPrefix = "/api/auth"
-
-/**
- * 로그인 후 리다이렉트되는 주소
- */
-export const DEFAULT_REDIRECT =
+const DEFAULT_REDIRECT =
   typeof window !== "undefined" ? window.location.href : ""
 
+/**
+ * 로그인 후 리다이렉트되는 주소.
+ * 클라이언트 사이드에서만 사용할 수 있음
+ */
 export const SIGNIN_WITH_CALLBACK = `/member/signin?callbackUrl=${DEFAULT_REDIRECT}`
