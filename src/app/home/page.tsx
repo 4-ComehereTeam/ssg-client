aee5438836519654c9a37bbbbd28599b2f8c6595
ddolboghi <rhdfyd128@gmail.com>
2024-04-16 09:00:28 +0900

    fix: 사용하지 않는 import문 삭제

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,12 @@
-import BottomNav from "@/components/BottomNav"
 import QuickMenu from "@/components/main/QuickMenu"
 import SearchBar from "@/components/ui/Headers/HeaderWithSearchBar"
 import TopNav from "@/components/main/TopNav"
 import AdBox from "@/components/main/AdBox"
 import MainEventSection from "@/components/main/MainEventSection"
-import Footer from "@/components/Footer"
 import SpecialPriceSection from "@/components/main/SpecialPriceSection"
 import HotItemsSection from "@/components/main/HotItemsSection"
+import { Suspense } from "react"
+import { Skeleton } from "@/components/ui/Skeleton"
 
 export default function Home() {
   return (
@@ -14,7 +14,9 @@ export default function Home() {
       <SearchBar />
       <TopNav index={"홈"} />
       <MainEventSection />
-      <QuickMenu />
+      <Suspense fallback={<Skeleton className="w-fit h-fit" />}>
+        <QuickMenu />
+      </Suspense>
       <AdBox />
       <SpecialPriceSection />
       <HotItemsSection />
