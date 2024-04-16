39d0e407d63612bb2fe341b3dda58895b24b48ba
ddolboghi <rhdfyd128@gmail.com>
2024-04-16 22:45:36 +0900

    fix: 사용하지 않는 코드 및 주석 제거

--- src/components/SpecailProduct.tsx
+++ src/components/SpecailProduct.tsx
@@ -1,9 +1,6 @@
-import React from "react"
-import CartIcon from "@/asset/images/CartIcon"
 import { ProductDataType } from "@/types/productDataType"
-import HeartIcon from "@/asset/images/HeartIcon"
-import { getSpecialPrice } from "@/actions/specialPrice/specialPrice"
 import Image from "next/image"
+import Link from "next/link"
 
 export default async function SpecialProduct({
   id,
@@ -31,43 +28,36 @@ export default async function SpecialProduct({
   return (
     <div className="static pt-[0.625rem] pb-5 mx-4 my-2.5">
       <div className="static">
-        <a href={src}>
+        <Link href={src}>
           <div className="relative">
-            <div className="overflow-hidden justify-center items-center ">
-              <div className="w-[100vw] h-[50vw]">
+            <div className=" justify-center items-center ">
+              <div className="aspect-video">
                 <Image
+                  className="object-contain"
                   src={imageUrl}
-                  fill={true}
                   alt={alt}
-                  className="will-change-auto max-w-[100%]"
+                  sizes="100vw"
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                  }}
+                  width={0}
+                  height={0}
+                  priority
                 />
               </div>
             </div>
           </div>
-        </a>
+        </Link>
       </div>
-      {/* --------좋아요/장바구니-------- */}
       <div className=" flex justify-between items-center pt-[0.125rem]">
-        {/* --------브랜드, 이름-------- */}
         <p className="text-ellipsis line-clamp-2 text-sm">
           <span className="font-bold mr-1">{brand}</span>
           {name}
         </p>
-        {/* --------브랜드, 이름-------- */}
-        <div className="flex justify-center">
-          <button className="inline-flex justify-center align-middle items-center w-7 h-7">
-            <div className="w-[20px] h-[20px]">
-              <HeartIcon />
-            </div>
-          </button>
-          <button className="inline-flex justify-center items-center w-7 h-7">
-            <CartIcon />
-          </button>
-        </div>
       </div>
       <p className="text-ellipsis line-clamp-2 text-sm">{subtitle}</p>
       <span className="font-bold">{addCommas(price)}원~</span>
-      {/* --------좋아요/장바구니-------- */}
       <a className="block mt-[0.625rem] pr-[1.25rem]"></a>
     </div>
   )
